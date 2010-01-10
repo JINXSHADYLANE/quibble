fc24c9be1863b77c9f773ff134ed5ea40134735c
Justinas Grazulis <as.justis@gmail.com>
2010-01-10 19:46:32 +0200

    Added game's handling with mouse

--- dgreed/apps/tor/game.c
+++ dgreed/apps/tor/game.c
@@ -10,6 +10,11 @@ uint game_tiles_total;
 Vector2 game_corner;
 GameState game_state;
 
+uint mouse_start_x, mouse_start_y;
+uint mouse_end_x, mouse_end_y;
+int dtile_x, dtile_y;
+uint tile_clicked;
+
 void game_init(void)
 {
 }
@@ -27,6 +32,7 @@ void game_reset(uint puzzle_num)
 	if(puzzle_state)
 		MEM_FREE(puzzle_state);
 	puzzle_state = MEM_ALLOC(game_tiles_total * sizeof(uint));
+	// ToDo: randomize puzzle start state
 	puzzle_state = memcpy(puzzle_state, game_puzzle.solved,
 					    game_tiles_total * sizeof(uint));
 
@@ -49,7 +55,7 @@ void game_render(void)
 	{
 		// Take i-th tile from original picture...
 		src = puzzle_get_tile_rect(game_puzzle_num, puzzle_state[i]);
-		
+	
 		// ... And place where it belongs
 		dest.left = (i % game_puzzle.width) * game_puzzle.tile_size.x +
 					 game_corner.x;
@@ -75,6 +81,24 @@ void game_update(void)
 	background_update();
 	
 	// algorithm to change current puzzle.
+	
+	// Initialize values?
+
+	if (mouse_down(MBTN_RIGHT))
+	{
+		mouse_pos(&mouse_start_x, &mouse_start_y);
+		// get tile number
+		tile_clicked = game_mouse_tile(mouse_start_x, mouse_start_y);
+	}
+	else if (mouse_up(MBTN_RIGHT))
+	{
+		mouse_pos(&mouse_end_x, &mouse_end_y);
+		dtile_x = game_delta_tile(mouse_start_x, mouse_end_x, game_puzzle.tile_size.x);
+		dtile_y = game_delta_tile(mouse_start_y, mouse_end_y, game_puzzle.tile_size.y);
+		
+		// ToDo: Animate rotation
+		game_rotate_board(dtile_x, dtile_y, tile_clicked);
+	}
 
 // If puzzle loaded and mouse just pressed:
 	//	 Take mouse coordinates
@@ -83,7 +107,6 @@ void game_update(void)
 	//	 Count_delta_coordinates()
 	//	 rotate_puzzle(row, col, direction)
 
-
 }
 
 void game_close(void)
@@ -91,5 +114,95 @@ void game_close(void)
 	if(puzzle_state)
 		MEM_FREE(puzzle_state);
 	puzzle_state = NULL;	
+}
+
+uint game_mouse_tile(uint mpos_x, uint mpos_y)
+{
+	mpos_x = mpos_x - game_corner.x;
+	mpos_y = mpos_y - game_corner.y;
+	
+	// Check if mouse position is out of board.
+	if (mpos_x >= (game_puzzle.tile_size.x * game_puzzle.width))
+		return -1;
+	if (mpos_y >= (game_puzzle.tile_size.y * game_puzzle.height))
+		return -1;
+	if (mpos_x < 0 || mpos_y < 0)
+		return -1;
+
+	uint row = mpos_y / game_puzzle.tile_size.y;
+	uint col = mpos_x / game_puzzle.tile_size.x;
+
+	return (row * game_puzzle.width + col);
+}
+
+int game_delta_tile(uint mpos0, uint mpos1, int tile_size)
+{
+	if (mpos1 > mpos0)
+	{
+		if (mpos1 - mpos0 >= tile_size) return 1;
+		else return 0;
+	}
+	else if (mpos1 < mpos0)
+	{
+		if (mpos0 - mpos1 >= tile_size) return -1;
+		else return 0;
+	}
+	return 0;
+}
+
+void game_rotate_board(int dtile_x, int dtile_y, uint tile_clicked)
+{
+	if (tile_clicked == -1) return;
+
+	uint row = tile_clicked / game_puzzle.width;
+	uint col = tile_clicked % game_puzzle.width;
+	uint row_first = game_puzzle.width * row;
+	uint row_last = game_puzzle.width * (row+1) - 1;
+	uint col_first = col;
+	uint col_last = (game_puzzle.height - 1) * game_puzzle.width + col;
+
+	assert(row_first < game_tiles_total);
+	assert(row_last < game_tiles_total);
+	assert(col_first < game_tiles_total);
+	assert(col_last < game_tiles_total);
+
+	// Rotates right
+	if (dtile_x == 1)
+	{
+		uint temp = puzzle_state[row_last]; 
+		for (uint i=row_last; i>row_first; i--)
+			puzzle_state[i] = puzzle_state[i-1];
+		puzzle_state[row_first] = temp;
+		return;
+	}
+	// Rotates left
+	if (dtile_x == -1)
+	{
+		uint temp = puzzle_state[row_first];
+		for (uint i=row_first; i<row_last; i++)
+			puzzle_state[i] = puzzle_state[i+1];
+		puzzle_state[row_last] = temp;
+		return;
+	}
+	// Rotates down
+	if (dtile_y == 1)
+	{
+		uint temp = puzzle_state[col_last];
+		for (uint i=col_last; i>col_first; i-=game_puzzle.width)
+			puzzle_state[i] = puzzle_state[i - game_puzzle.width];
+		puzzle_state[col_first] = temp;
+		return;
+	}
+	// Rotates up
+	if (dtile_y == -1)
+	{
+		uint temp = puzzle_state[col_first];
+		for (uint i=col_first; i<col_last; i+=game_puzzle.width)
+			puzzle_state[i] = puzzle_state[i + game_puzzle.width];
+		puzzle_state[col_last] = temp;
+		return;
+	}
+	
+	return;
 }
 
