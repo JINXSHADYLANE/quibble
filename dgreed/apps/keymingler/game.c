62f1f4cc55730edfc39b72ae73594ce5ef2651ea
Andrius Mitkus <andrius.mitkus@gmail.com>
2010-01-30 11:00:43 +0200

    Proper barrel destruction by typing

--- dgreed/apps/keymingler/game.c
+++ dgreed/apps/keymingler/game.c
@@ -18,6 +18,8 @@ FontHandle font;
 
 // Tweakables
 float barrel_fall_speed = 100.0f;
+float barrel_drop_interval = 3.0f;
+float barrel_drop_variance = 1.5f;
 
 // Globals
 #define MAX_BARRELS 64
@@ -54,8 +56,23 @@ void _draw_barrel(Vector2 pos, char letter) {
 }
 
 void _update_barrels(float dt) {
-	
+	// Count how many chars were pressed
+	uint char_count = 0;
+	for(char c = 'a'; c <= 'z'; ++c) {
+		if(char_down(c))
+			char_count++;
+	}
+
 	for(int i = 0; i < barrel_count; ++i) {
+		// Check for correct keypresses	
+		if(char_down(barrels[i].letter)) {
+			// TODO: explode barrel
+			
+			barrels[i] = barrels[barrel_count-1];
+			barrel_count--;
+			i--;
+		}
+
 		barrels[i].pos.y += dt *  barrel_fall_speed;
 		if(barrels[i].pos.y > SCREEN_HEIGHT - 100.0f) {
 			// Destroy barrel
@@ -64,6 +81,21 @@ void _update_barrels(float dt) {
 			i--;
 		}
 	}
+}
+
+void _generate_barrels(float t) {
+	static float last_drop = 0.0f;
+	static float next_in = 0.0f;
+
+	if(t - last_drop > next_in) {
+		_drop_barrel();
+		next_in = 0.0f;
+		last_drop = t;
+	}
+
+	if(next_in == 0.0f) 
+		next_in = barrel_drop_interval 
+			+ rand_float_range(-barrel_drop_variance, barrel_drop_variance);
 }
 
 void game_init(void) {
@@ -81,14 +113,15 @@ void game_close(void) {
 }
 
 void game_update(void) {
+	float t = time_ms() / 1000.0f;
 	float dt = time_delta() / 1000.0f;
 
+	_generate_barrels(t);
 	_update_barrels(dt);
-	if(char_down('a'))
-		_drop_barrel();
 }
 
 void game_render(void) {
 	for(uint i = 0; i < barrel_count; ++i)
 		_draw_barrel(barrels[i].pos, barrels[i].letter);
 }
+
