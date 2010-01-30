3a4763d8d20e4a6c7c6f26fd596a4e4a551280f1
Andrius Mitkus <andrius.mitkus@gmail.com>
2010-01-30 10:00:26 +0200

    Barrels are falling

--- dgreed/apps/keymingler/game.c
+++ dgreed/apps/keymingler/game.c
@@ -1,12 +1,38 @@
 #include "game.h"
 #include "common.h"
 #include <system.h>
+#include <utils.h>
 #include <font.h>
 
+// Types
+typedef struct {
+	Vector2 pos;
+	char letter;
+} Barrel;	
+
+// Resources
 TexHandle tex_barrel;
 RectF rect_barrel = {0, 0, 70, 84};
 
 FontHandle font;
+
+// Tweakables
+float barrel_fall_speed = 100.0f;
+
+// Globals
+#define MAX_BARRELS 64
+uint barrel_count;
+Barrel barrels[MAX_BARRELS];
+
+void _drop_barrel(void) {
+	if(barrel_count == MAX_BARRELS)
+		LOG_ERROR("Too many barrels!");
+
+	barrels[barrel_count].pos = vec2(rand_float_range(20.0f, SCREEN_WIDTH - 20.0f),
+		-40.0f);	
+	barrels[barrel_count].letter = (char)rand_int((int)'a', (int)'z');	
+	barrel_count++;
+}
 
 void _draw_barrel(Vector2 pos, char letter) {
 	char str[] = {letter, '\0'};
@@ -27,10 +53,25 @@ void _draw_barrel(Vector2 pos, char letter) {
 	font_draw(font, str, 1, &letter_pos, COLOR_BLACK);
 }
 
+void _update_barrels(float dt) {
+	
+	for(int i = 0; i < barrel_count; ++i) {
+		barrels[i].pos.y += dt *  barrel_fall_speed;
+		if(barrels[i].pos.y > SCREEN_HEIGHT - 100.0f) {
+			// Destroy barrel
+			barrels[i] = barrels[barrel_count-1];
+			barrel_count--;
+			i--;
+		}
+	}
+}
+
 void game_init(void) {
 	tex_barrel = tex_load(BARREL_FILE);
 
 	font = font_load(FONT_FILE);
+
+	barrel_count = 0;
 }
 
 void game_close(void) {
@@ -40,8 +81,14 @@ void game_close(void) {
 }
 
 void game_update(void) {
+	float dt = time_delta() / 1000.0f;
+
+	_update_barrels(dt);
+	if(char_down('a'))
+		_drop_barrel();
 }
 
 void game_render(void) {
-	_draw_barrel(vec2(100.0f, 100.0f), 'a');
+	for(uint i = 0; i < barrel_count; ++i)
+		_draw_barrel(barrels[i].pos, barrels[i].letter);
 }
--- dgreed/apps/keymingler/keymingler.c
+++ dgreed/apps/keymingler/keymingler.c
@@ -1,10 +1,12 @@
 #include <system.h>
+#include <utils.h>
 #include "common.h"
 #include "layouts.h"
 #include "game.h"
 
 int main(int argc, const char** argv) {
 	log_init("keymingler.log", LOG_LEVEL_INFO);
+	rand_init(52890);
 	layouts_init();
 	video_init(SCREEN_WIDTH, SCREEN_HEIGHT);
 	game_init();
