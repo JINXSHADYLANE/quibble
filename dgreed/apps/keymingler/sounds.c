74b8ecb27990a57d3242c20bd0dc5fbfaa55801a
Andrius Mitkus <andrius.mitkus@gmail.com>
2010-01-31 03:54:14 +0200

    New images and sounds

--- dgreed/apps/keymingler/sounds.c
+++ dgreed/apps/keymingler/sounds.c
@@ -5,10 +5,12 @@
 #define BURN_FILE ASSETS_DIR "sudega.wav"
 #define BULBUL_FILE ASSETS_DIR "bulbul.wav"
 #define SINKED_FILE ASSETS_DIR "issilieja.wav"
+#define MISSED_FILE ASSETS_DIR "juokas.wav"
 
 SoundHandle sound_burning;
 SoundHandle sound_bulbul;
 SoundHandle sound_sinked;
+SoundHandle sound_missed;
 
 void sounds_init(void)
 {
@@ -17,6 +19,7 @@ void sounds_init(void)
 	sound_burning = sound_load_sample(BURN_FILE);
 	sound_bulbul = sound_load_sample(BULBUL_FILE);
 	sound_sinked = sound_load_sample(SINKED_FILE);
+	sound_missed = sound_load_sample(MISSED_FILE);
 }
 
 void sounds_close(void)
@@ -24,7 +27,8 @@ void sounds_close(void)
 	sound_free(sound_burning);
 	sound_free(sound_bulbul);
 	sound_free(sound_sinked);
-
+	sound_free(sound_missed);
 
 	sound_close();
-}	
+}
+
--- dgreed/apps/keymingler/sounds.h
+++ dgreed/apps/keymingler/sounds.h
@@ -6,9 +6,11 @@
 extern SoundHandle sound_burning;
 extern SoundHandle sound_bulbul;
 extern SoundHandle sound_sinked;
+extern SoundHandle sound_missed;
 
 void sounds_init(void);
 void sounds_close(void);
 
 
 #endif
+
