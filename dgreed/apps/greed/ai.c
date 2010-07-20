db2880e841402f64d57d595f3b02bcd9d54a73d7
Andrius Mitkus <andrius.mitkus@gmail.com>
2010-07-20 16:47:45 +0300

    greed: reduced navmesh size on disk and memory by ~40%, fixed floyd-warshall distance precalc, optimized arena builder is now used by default

--- dgreed/apps/greed/ai.c
+++ dgreed/apps/greed/ai.c
@@ -13,24 +13,26 @@ void ai_debug_draw() {
 
 	for(uint i = 0; i < n; ++i) {
 		Vector2 v1 = nav_mesh->navpoints[i];
-		for(uint j = i+1; j < n; ++j) {
-			Vector2 v2 = nav_mesh->navpoints[j];
-			if(nav_mesh->adjacency[IDX_2D(i, j, n)] > 0.0f) { 
-				Segment s = ai_shortest_path(v1, v2);
-				Segment s1, s2;
-				if(ai_split_path(s, &s1, &s2)) {
-					video_draw_line(DEBUG_DRAW_LAYER + i%2, 
-						&s1.p1, &s1.p2, COLOR_WHITE);
-					video_draw_line(DEBUG_DRAW_LAYER + i%2, 
-						&s2.p1, &s2.p2, COLOR_WHITE);
-				}
-				else {
-					video_draw_line(DEBUG_DRAW_LAYER + i%2,
-						&s1.p1, &s1.p2, COLOR_WHITE);
-				}		
-			}	
+		uint start = nav_mesh->neighbours_start[i];
+		uint count = nav_mesh->neighbour_count[i];
+		for(uint j = start; j < start + count; ++j) {
+			uint i2 = nav_mesh->neighbours[j];
+			if(i2 < i)
+				continue;
+			Vector2 v2 = nav_mesh->navpoints[i2];
+			Segment s = ai_shortest_path(v1, v2);
+			Segment s1, s2;
+			if(ai_split_path(s, &s1, &s2)) {
+				video_draw_line(DEBUG_DRAW_LAYER + i%2, 
+					&s1.p1, &s1.p2, COLOR_WHITE);
+				video_draw_line(DEBUG_DRAW_LAYER + i%2, 
+					&s2.p1, &s2.p2, COLOR_WHITE);
+			}
+			else {
+				video_draw_line(DEBUG_DRAW_LAYER + i%2,
+					&s1.p1, &s1.p2, COLOR_WHITE);
+			}		
 		}
-		//gfx_draw_point(DEBUG_DRAW_LAYER, &nav_mesh->navpoints[i], COLOR_WHITE);
 	}
 }
 
