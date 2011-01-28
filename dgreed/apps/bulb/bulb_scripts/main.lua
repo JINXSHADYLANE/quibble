2822f48282859cfd44d32a9ddb0a4f07d1adbbf3
Andrius Mitkus <andrius.mitkus@gmail.com>
2011-01-29 01:03:45 +0200

    bulb: Global GameJam '11 game

---
+++ dgreed/apps/bulb/bulb_scripts/main.lua
@@ -0,0 +1,37 @@
+-- sausra 
+
+screen = rect(0, 0, 960, 640)
+pre, src = 'bulb_assets/', 'bulb_scripts/'
+
+dofile(src..'game.lua')
+
+function init()
+        sound.init()
+        video.init(screen.r, screen.b, 'bulb')
+
+        game.init()
+end
+
+function close()
+        game.close()
+
+        video.close()
+        sound.close()
+end
+
+function frame()
+        game.frame()
+        return not key.pressed(key.quit) 
+end
+
+function main()
+        init()
+        repeat
+                cont = frame()
+                sound.update()
+        until not video.present() or not cont
+        close()
+end
+
+main()
+
