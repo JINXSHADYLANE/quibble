51ca6a1055111880b49e3b05507c5f98d365fdad
Andrius Mitkus <andrius.mitkus@gmail.com>
2011-01-30 09:29:17 +0200

    bulb: new sounds, tweaks

--- dgreed/apps/bulb/bulb_scripts/eyes.lua
+++ dgreed/apps/bulb/bulb_scripts/eyes.lua
@@ -60,9 +60,9 @@ function eyes.update(lights)
 			end
 		end
 
-		if length(e.p - robo.pos) > 700 then
-			if rand.int(0, 50) == 42 then
-				run_vect = run_vect + normalize(robo.pos - e.p) * 400
+		if length(e.p - robo.pos) > 500 then
+			if rand.int(0, 43) == 42 then
+				run_vect = run_vect + normalize(robo.pos - e.p) * 800
 			end
 		end
 
