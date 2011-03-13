e3c1bb5e47a76002a28b3e0c364bd3cb7cb4e199
Andrius Mitkus <andrius.mitkus@gmail.com>
2011-03-13 21:28:08 +0200

    greed: arena image generator

--- dgreed/tools/greed_arena_list.py
+++ dgreed/tools/greed_arena_list.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python2
 
+from PIL import Image, ImageDraw, ImageChops
 from optparse import OptionParser
 import os, os.path, re
 import sys
 import mml
+
+ring_img, ring_shadow, core_img, core_shadow = None, None, None, None
+matlas = None
 
 if sys.platform == 'win32':
 	separator = '\\'
@@ -51,6 +55,7 @@ def build_arena_list(chapters_list, outfile):
 
 	for chapter in chapters_list:
 		name = filename(chapter[0])[:2]
+		render_arenas(chapter, 'atlas_' + name + '.png')
 		chapter_node = mml.Node('chapter', name)
 		for arena in chapter:
 			contents = open(arena, 'r')
@@ -72,6 +77,90 @@ def build_arena_list(chapters_list, outfile):
 	out.write(mml.serialize(root))
 	out.close()
 
+def parse_vector(string):
+	s = string.split(',')
+	return (float(s[0]), float(s[1]))
+
+def render_arenas(chapter, outfile):
+	img = Image.new('RGBA', (1024, 512), (0, 0, 0, 0))
+	cursor_x, cursor_y = 1, 1
+	for arena in chapter:
+		contents = open(arena, 'r')
+		tree = mml.deserialize(contents.read())
+		contents.close()
+
+		arena_img = render_arena(tree)
+		
+		if cursor_x + 338 >= 1024:
+			cursor_x = 1
+			cursor_y += 226
+		
+		img.paste(arena_img, (cursor_x, cursor_y))
+		
+		cursor_x += 339
+
+	img.save(outfile)
+
+
+def render_arena(desc):
+	background, walls = None, None
+	shadow_shift = (0, 0)
+	platforms = []
+
+	for child in desc.children:
+		if child.name == 'background':
+			background = Image.open(filename(child.value))
+		if child.name == 'walls':
+			walls = Image.open(filename(child.value))
+		if child.name == 'shadow_shift':
+			shadow_shift = parse_vector(child.value)
+		if child.name == 'platforms':
+			for platform in child.children:
+				platforms.append(parse_vector(platform.value))
+	
+	if background == None or walls == None or len(platforms) == 0:
+		raise RuntimeError('Unable to render arena - bad desc')
+
+	img = Image.new('RGBA', (480, 320), (0, 0, 0, 0))
+	img.paste(background.crop((0, 0, 480, 320)))
+	
+	walls_img = walls.crop((0, 0, 480, 320))
+	shadow_img = walls.crop((0, 321, 240, 321 + 160))
+	shadow_img = shadow_img.resize((480, 320), Image.BICUBIC)
+	shadow_img = ImageChops.offset(shadow_img, int(shadow_shift[0]), int(shadow_shift[1]))
+	
+	b = background.crop((0, 0, 480, 320))
+	img = Image.composite(img, shadow_img, ImageChops.invert(shadow_img))
+	img.putalpha(b.split()[-1])
+	
+	# Platforms
+	for p in platforms:
+		x, y = int(p[0]) - 26, int(p[1]) - 26			
+		r_img = ImageChops.offset(ring_img, x, y)
+		c_img = ImageChops.offset(core_img, x, y)
+		x, y = x + int(shadow_shift[0]), y + int(shadow_shift[1])
+		r_shd = ImageChops.offset(ring_shadow, x, y)
+		c_shd = ImageChops.offset(core_shadow, x, y)
+		img = Image.composite(img, r_shd, ImageChops.invert(r_shd))
+		img = Image.composite(img, c_shd, ImageChops.invert(c_shd))
+		img = Image.composite(img, r_img, ImageChops.invert(r_img))
+		img = Image.composite(img, c_img, ImageChops.invert(c_img))
+		img.putalpha(b.split()[-1])
+	
+	img = Image.composite(img, walls_img, ImageChops.invert(walls_img))
+	img.putalpha(b.split()[-1])
+
+
+	img = img.resize((338, 225), Image.ANTIALIAS)
+	b = img.copy()
+	img = Image.composite(img, matlas, ImageChops.invert(matlas))
+	img.putalpha(matlas.split()[-1])
+	bands = img.split()
+	max_alpha = bands[-1].getextrema()[1]
+	bands_a = Image.eval(bands[-1], lambda x: int(x / float(max_alpha) * 255.0))
+	img = Image.merge('RGBA', (bands[0], bands[1], bands[2], bands_a))
+	return img	
+
 if __name__ == '__main__':
 	usage = '%prog DEST'
 	parser = OptionParser(usage=usage)
@@ -79,6 +168,20 @@ if __name__ == '__main__':
 	(opt, args) = parser.parse_args(sys.argv)
 	if len(args) != 2:
 		parser.error('You must provide destination file')
+
+	atlas = Image.open('atlas1.png')
+	matlas = Image.open('menu_atlas.png')
+	matlas = matlas.crop((0, 0, 338, 225))
+
+	ring_img = Image.new('RGBA', (480, 320), (0, 0, 0, 0))
+	ring_shadow = Image.new('RGBA', (480, 320), (0, 0, 0, 0))
+	core_img = Image.new('RGBA', (480, 320), (0, 0, 0, 0))
+	core_shadow = Image.new('RGBA', (480, 320), (0, 0, 0, 0))
+
+	ring_img.paste(atlas.crop((360, 360, 360 + 52, 360 + 52)), (0, 0))
+	ring_shadow.paste(atlas.crop((433, 360, 433 + 52, 360 + 52)), (0, 0))
+	core_img.paste(atlas.crop((1, 433, 1 + 40, 433 + 40)), (6, 6))
+	core_shadow.paste(atlas.crop((144, 431, 144 + 45, 431 + 45)), (3, 3))
 
 	chapters = scan()
 	build_arena_list(chapters, args[1])
