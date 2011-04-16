4ce921bc54e32b0fb6f4d2b01305fadd0c53aa1e
Justinas Gra?ulis <as.justis@gmail.com>
2011-04-16 08:38:14 +0300

    gurbas: added block animations

--- dgreed/apps/gurbas/gurbas_scripts/block.lua
+++ dgreed/apps/gurbas/gurbas_scripts/block.lua
@@ -10,8 +10,12 @@ block = {
 	shape = block_shapes[1],
 	-- coordinates in map tiles
 	offset = vec2(),
-	-- animation offset
-	anim_offset = vec2()
+	-- time when offset position was last changed
+	off_t = 0,
+	-- how much time tile animation takes
+	fall_time = 240,
+	-- 1=left, 2=up, 3=right, 4=down, 0=none
+	animate = 0
 }
 
 function block.init()
@@ -25,27 +29,65 @@ function block.close()
 end
 
 function block.update()
-	-- playing with tiles
-	if key.pressed(key._up) then
-		block.shape = block_shapes[2]
+	-- Update keyboard arrows
+	if key.down(key._left) then
+		block.animate = 1
+		block.off_t = time.ms()
+	elseif key.down(key._up) then
+		block.animate = 2
+		block.off_t = time.ms()
+	elseif key.down(key._right) then
+		block.animate = 3
+		block.off_t = time.ms()
+	elseif key.down(key._down) then
+		block.animate = 4
+		block.off_t = time.ms()
 	end
-	if key.pressed(key._down) then
-		block.shape = block_shapes[1]
+
+	-- check if we can change block position
+	if (time.ms() - block.off_t) / block.fall_time >= 1.0 then
+		if block.animate == 1 then
+			block.offset.x = block.offset.x - 1
+		elseif block.animate == 2 then
+			block.offset.y = block.offset.y - 1
+		elseif block.animate == 3 then
+			block.offset.x = block.offset.x + 1
+		elseif block.animate == 4 then
+			block.offset.y = block.offset.y + 1
+		end
+
+		block.animate = 0
+		block.off_t = time.ms()
 	end
+
 end
 
 function block.draw()
 	for id, tile in ipairs(block.shape) do
+		-- count animation offset between two fixed positions
+		local t = (time.ms() - block.off_t) / block.fall_time
+		
 		-- count tile position in pixels
-		local dest = (block.offset + tile) * tile_size
-		video.draw_rect(block.tex, 1, dest)
+		local dest = (block.offset + tile)
+
+		if block.animate == 1 then
+			dest.x = lerp(dest.x, dest.x - 1, t)
+		elseif block.animate == 2 then
+			dest.y = lerp(dest.y, dest.y - 1, t)
+		elseif block.animate == 3 then
+			dest.x = lerp(dest.x, dest.x + 1, t)
+		elseif block.animate == 4 then
+			dest.y = lerp(dest.y, dest.y + 1, t)
+		end
+
+		video.draw_rect(block.tex, 1, dest * tile_size)
 	end
  
 end
 
 function block.reset()
 	block.offset = vec2(0, 0)
-	block.anim_offset = vec2(0, 0)
+	block.off_t = time.ms()
 	-- pick random block
 end
 
