8d771984e99c9dbdc62fca401a0b2d9557c5123a
Andrius Mitkus <andrius.mitkus@gmail.com>
2011-08-23 23:17:08 +0300

    malka: modules, initial design for states system

--- dgreed/src/malka/malka.c
+++ dgreed/src/malka/malka.c
@@ -4,14 +4,16 @@
 #include "ml_system.h"
 #include "ml_mml.h"
 
-#ifdef MACOSX_BUNDLE
 #include "memory.h"
+
+#ifdef MACOSX_BUNDLE
 #include <unistd.h>
 #endif
 
 static lua_State* l;
 static int ml_argc = 0;
 static const char** ml_argv = NULL;
+static bool ml_prepped = false;
 
 extern bool fs_devmode;
 
@@ -78,7 +80,7 @@ int malka_register(bind_fun_ptr fun) {
 	return (*fun)(l);
 }
 
-int malka_run_ex(const char* luafile) {
+static void _malka_prep(const char* luafile) {
 	// If we're in Mac OS X bundle, some rituals need to be performed
 	#ifdef MACOSX_BUNDLE
 	char* real_path = path_to_resource(luafile);
@@ -98,6 +100,15 @@ int malka_run_ex(const char* luafile) {
 	MEM_FREE(real_folder);
 	#endif
 
+	// Register module path
+	char* module_path = path_get_folder(luafile);
+	lua_getglobal(l, "package"); 
+	int package = lua_gettop(l);
+	lua_pushfstring(l, "./%s?.lua", module_path);
+	lua_setfield(l, package, "path");
+	lua_pop(l, 1);
+	MEM_FREE(module_path);
+
 	// Register params
 	if(ml_argv) {
 		lua_createtable(l, ml_argc, 0);
@@ -114,12 +125,25 @@ int malka_run_ex(const char* luafile) {
 	}
 	lua_setfield(l, LUA_GLOBALSINDEX, "argv");
 
+	ml_prepped = true;
+}
+
+int malka_run_ex(const char* luafile) {
+	assert(luafile);
+
+	_malka_prep(luafile);
+
 	if(luaL_dofile(l, luafile)) {
 		const char* err = luaL_checkstring(l, -1);
 		LOG_WARNING("error in lua script:\n%s\n", err);
 		printf("An error occured:\n%s\n", err);
 	}
 
+	return 0;
+}
+
+int malka_states_run(const char* luafile) {
+	// TBD
 	return 0;
 }
 
