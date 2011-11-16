9981e1967b3f64b80b9aaa3ebe11342735575c31
Justina <j.mickonyte@gmail.com>
2011-11-17 01:58:17 +0200

    ldrb: working version, some tests

--- dgreed/src/ldrb/records/tests.py
+++ dgreed/src/ldrb/records/tests.py
@@ -1,11 +1,49 @@
 from django.test import TestCase
 from records.models import record
+from random import *
+import datetime
+
+
+def setUpDB(n):
+	Entries = []
+	seed(datetime.datetime.now())
+	for i in range(n):
+		rDate = datetime.datetime(2011, int(random()*10)+1, int(random()*27)+1, int(random()*23)+1, int(random()*59)+1, int(random()*59)+1 )
+		Entries.append( record(gid='rthief', uid='dwarf01', weight=int(random()*50), time=rDate, data='adksf6565d55dddd5d64csd5sd4')  )		
+	for e in Entries:
+		e.save()
 
 class RecordsTestCase(TestCase):	
+
 	def test_put(self):
-		response = self.client.post('/records/put/', POST='(record r (gid rthief) (uid seklys_morka) (weight 3) (data fhdjh5254557y7765b550fhd5HBe74&*6))', content_type='text', follow=False, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		setUpDB(3)
+		print 'PUT TEST'
+		dataString0 = '(entry _ (gid plesiku_urvas) (uid seklys_morka) (weight 3) (data fhdjh5254557y7765b550fhd5HBe74&*6))'
+		dataString1 = '(entry _ (gid guitarLoser) (uid prawn33) (weight 559) (data fhdjh5254557y7765b550fhd5HBe74&*6))'
+		dataString2 = '(entry _ (gid miau!) (uid seklys_morka) (weight 6) (data fhdjh5254557y7765b550fhd5HBe74&*6))'
+
+		response = self.client.post('/records/put/', raw_post_data=dataString2, mimetype='text/mml', follow=False, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+
+		for i in record.objects.values_list():
+			print i
+		print '\n\n'
+
 		self.assertEqual(response.status_code, 200)
 		
+
+
+	def test_query(self):
+		setUpDB(10)
+		print 'QUERY TEST'
+		queryString0 = '(query _ (gid rthief) (startEntry 0) (entryCount 10) (startTime 20111116113710) (endTime 20111116133710))'
+		queryString1 = '(query _ (gid rthief) (startEntry 0) (entryCount 14) (startTime 20110716113710) (endTime 20111016133710))'
+		queryString2 = '(query _ (gid rthief) (startEntry 0) (entryCount 2) (startTime 20110716113710) (endTime 20110716113710))'
+		queryString3 = '(query _ (gid rthief) (startEntry 0) (entryCount 2) (endTime 20110716113710))'
+
+		response = self.client.get('/records/query/', QUERY_STRING=queryString2, mimetype='text/mml', follow=False, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+
+		print 'query response:\n', response
+		self.assertEqual(response.status_code, 200)
 
 # run:  python manage.py test records
 		
--- dgreed/src/ldrb/records/views.py
+++ dgreed/src/ldrb/records/views.py
@@ -1,15 +1,19 @@
 from models import record
 from django.http import HttpRequest, HttpResponse
-import mml
 import datetime
+import time
+import mml
 
 
 def put(request):
 	
-	print 'request: ', request , ' -- end'
-	#print 'POST:', request.META['POST']
+	print 'Put: request: ', request , ' -- end\n'
 
-	tree = mml.deserialize(request.META['POST'])	
+	tree = mml.deserialize(request.META['raw_post_data'])
+	if len(tree.children) != 4:
+		print 'Bad entry format. 4 fields required, got ', len(tree.children)
+		return
+
 	fields = dict( [(tree.children[i].name, tree.children[i].value) for i in range(4)] )
 
 	r = record()
@@ -21,19 +25,48 @@ def put(request):
 	r.save()
 	
 	response = HttpResponse()
-	response.status_code = 200 # OK
+	response.status_code = 200 # ok	
 	return response
 
 
+
 def query(request):
+	print '\nQuery: request: ', request , ' -- end\n'
 
-	req = mml.deserialize(request.raw_post_data)
+	tree = mml.deserialize(request.META['QUERY_STRING'])
+	if len(tree.children) != 5:
+		print 'Bad query format. 5 fields required, got', len(tree.children)
+		return
 
-	result = record.objects.filter(time > req[2].value and time < req[3].value).order_by('weight')
-	result = result[ :req[1].value ]  # take first n values
+	for i in record.objects.values_list():
+		print i
+	print '\n'
+
+	fields = dict( [(tree.children[i].name, tree.children[i].value) for i in range(5)] )
 	
-	response = HttpResponse()
-	# write result to response
+	startTime = datetime.datetime(*time.strptime(fields['startTime'], '%Y%m%d%H%M%S')[0:6] )
+	endTime = datetime.datetime(*time.strptime(fields['endTime'], '%Y%m%d%H%M%S')[0:6] ) 
+	
+	results = record.objects.filter(gid=fields['gid'])
+	if startTime < endTime:
+		results = results.filter(time__gt=startTime).filter(time__lt=endTime)
+	elif startTime > endTime:
+		print 'startTime > endTime'
+	results = results.order_by('weight')[fields['startEntry'] : fields['startEntry'] + fields['entryCount']]
+
+	resp = mml.Node( 'entries', str(len(results)) )
+	for r in results:
+		entry = (mml.Node( 'entry', '_'))
+		entry.children.append(mml.Node('gid', r.gid))
+		entry.children.append(mml.Node('uid', r.uid))
+		entry.children.append(mml.Node('time', r.time.strftime('%Y%m%d%H%M%S')))
+		entry.children.append(mml.Node('weight', str(r.weight)) )
+		entry.children.append(mml.Node('data', r.data))
+		resp.children.append(entry)
+
+	response = HttpResponse(mimetype="text/mml")
+
+	response.write(mml.serialize(resp))
 	response.status_code = 200 # ok	
 	return response
 
