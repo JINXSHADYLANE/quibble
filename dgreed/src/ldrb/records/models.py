0cb9a9795efd9ee9eb4af2df8167c04377f409b0
Justina <j.mickonyte@gmail.com>
2011-11-17 01:58:17 +0200

    ldrb: working version, some tests

--- dgreed/src/ldrb/records/models.py
+++ dgreed/src/ldrb/records/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-import base64
 import datetime
 
 class record(models.Model):
@@ -9,22 +8,7 @@ class record(models.Model):
 	weight = models.IntegerField()
 	data = models.TextField()
 
-	'''def set_data(self, data):
-		self._data = base64.encodestring(data)
-	def get_data(self):
-		return base64.decodestring(self._data)
-	data = property(get_data, set_data)'''
-
 	def __unicode__(self):
 		return self.uid
-
-
-'''class log(models.Model):
-	gid = models.CharField(max_length=10)
-	uid = models.CharField(max_length=20)
-	time = models.DateTimeField('date published')
-	action = models.CharField(max_length=50)
-	def __unicode__(self):
-		return self.action'''
 
 
