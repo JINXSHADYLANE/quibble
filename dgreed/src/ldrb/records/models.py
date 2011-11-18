0761bffd766bd97d27921112762e667f1d8afca1
Justina <j.mickonyte@gmail.com>
2011-11-18 17:31:11 +0200

    ldrb: exception handling, tests

--- dgreed/src/ldrb/records/models.py
+++ dgreed/src/ldrb/records/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 import datetime
 
+ID_LENGTH=50
+
 class record(models.Model):
-	gid = models.CharField(max_length=50)
-	uid = models.CharField(max_length=50)
+	gid = models.CharField(max_length=ID_LENGTH)
+	uid = models.CharField(max_length=ID_LENGTH)
 	time = models.DateTimeField('date published')
 	weight = models.IntegerField()
 	data = models.TextField()
--- dgreed/src/ldrb/records/tests.py
+++ dgreed/src/ldrb/records/tests.py
@@ -15,35 +15,59 @@ def setUpDB(n):
 
 class RecordsTestCase(TestCase):	
 
-	def test_put(self):
+	def test_put0(self):  # good dataString
 		setUpDB(3)
-		print 'PUT TEST'
-		dataString0 = '(entry _ (gid plesiku_urvas) (uid seklys_morka) (weight 3) (data fhdjh5254557y7765b550fhd5HBe74&*6))'
-		dataString1 = '(entry _ (gid guitarLoser) (uid prawn33) (weight 559) (data fhdjh5254557y7765b550fhd5HBe74&*6))'
-		dataString2 = '(entry _ (gid miau!) (uid seklys_morka) (weight 6) (data fhdjh5254557y7765b550fhd5HBe74&*6))'
+		#print 'PUT TEST'
+		dataString = '(entry _ (gid plesiku_urvas) (uid seklys_morka) (weight 3) (data fhdjh5254557y7765b550fhd5HBe74&*6))'
+		response = self.client.post('/records/put/', raw_post_data=dataString, mimetype='text/plain', follow=False, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		#for i in record.objects.values_list():
+			#print i
+		#print '\n\n'
+		self.assertEqual(response.status_code, 201)
 
-		response = self.client.post('/records/put/', raw_post_data=dataString2, mimetype='text/mml', follow=False, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+	def test_put1(self):  # good dataString
+		setUpDB(3)
+		dataString = '(entry _ (gid guitarLoser) (uid prawn33) (weight 559) (data fhdjh5254557y7765b550fhd5HBe74&*6))'
+		response = self.client.post('/records/put/', raw_post_data=dataString, mimetype='text/plain', follow=False, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		self.assertEqual(response.status_code, 201)
 
-		for i in record.objects.values_list():
-			print i
-		print '\n\n'
+	def test_put2(self): # no dataString
+		setUpDB(3)
+		response = self.client.post('/records/put/' , mimetype='text/plain', follow=False, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		self.assertEqual(response.status_code, 400)
 
-		self.assertEqual(response.status_code, 200)
-		
+
+	def test_put3(self): # not enough fields
+		setUpDB(3)
+		dataString = '(entry _ (gid miau!) (weight 32800) (data fhdjh5254557y7765b550fhd5HBe74&*6))'
+		response = self.client.post('/records/put/', raw_post_data=dataString, mimetype='text/plain', follow=False, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		self.assertEqual(response.status_code, 400)
+
+	def test_put4(self): # messy dataString
+		setUpDB(3)
+		dataString = '(entry _ (gid miau! (uid seklys_morka) [weight 32800] (data fhdjh5254557y7765b550fhd5HBe74&*6) (baubas))'
+		response = self.client.post('/records/put/', raw_post_data=dataString, mimetype='text/plain', follow=False, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		self.assertEqual(response.status_code, 400)
+
+	def test_put5(self): # empty dataString
+		setUpDB(3)
+		dataString = ''
+		response = self.client.post('/records/put/', raw_post_data=dataString, mimetype='text/plain', follow=False, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		self.assertEqual(response.status_code, 400)
 
 
 	def test_query(self):
 		setUpDB(10)
-		print 'QUERY TEST'
+		#print 'QUERY TEST'
 		queryString0 = '(query _ (gid rthief) (startEntry 0) (entryCount 10) (startTime 20111116113710) (endTime 20111116133710))'
 		queryString1 = '(query _ (gid rthief) (startEntry 0) (entryCount 14) (startTime 20110716113710) (endTime 20111016133710))'
 		queryString2 = '(query _ (gid rthief) (startEntry 0) (entryCount 2) (startTime 20110716113710) (endTime 20110716113710))'
 		queryString3 = '(query _ (gid rthief) (startEntry 0) (entryCount 2) (endTime 20110716113710))'
 
-		response = self.client.get('/records/query/', QUERY_STRING=queryString2, mimetype='text/mml', follow=False, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+		response = self.client.get('/records/query/', QUERY_STRING=queryString0, mimetype='text/plain', follow=False, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
 
 		print 'query response:\n', response
-		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.status_code, 201)
 
 # run:  python manage.py test records
 		
