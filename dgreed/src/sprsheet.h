1fd93b3549f6c5d38016a1491f148fde75e1ae6b
Andrius Mitkus <andrius.mitkus@gmail.com>
2012-01-05 18:48:04 +0200

    dgreed: sprsheet desc example

--- dgreed/src/sprsheet.h
+++ dgreed/src/sprsheet.h
@@ -6,9 +6,37 @@
 // A layer on top of textures to stop worrying about loading, freeing 
 // and source rectangles.
 
+/*
+Example description:
+
+(sprsheet bulb
+	(prefix "bulb_assets/")
+	(preload background,title,robot)
+	
+	(img background
+		(tex "background.png")
+	)
+
+	(img title
+		(tex "atlas.png")
+		(src 0,0,480,64)
+	)
+
+	(anim robot
+		(tex "robot.png")
+		(frames 12)
+		(src 0,0,32,48) # first frame
+		(offset 34,50) # offset for next frames
+	)
+)
+
+*/
+
 void sprsheet_init(const char* desc);
 void sprsheet_close(void);
 
-bool sprsheet_get(const char* name, TexHandle* handle, RectF* src);
+void sprsheet_get(const char* name, TexHandle* handle, RectF* src);
+void sprsheet_get_anim(const char* name, uint frame, TexHandle* handle, RectF* src);
+int sprsheet_get_anim_frames(const char* name);
 
 #endif
