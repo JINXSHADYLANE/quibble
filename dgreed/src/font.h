a08e99816f7480265266228c9ef3bc458ae05bd9
Domas Markevicius <domas.marko@gmail.com>
2012-01-30 16:18:48 +0200

    nulis2: new sounds

---
+++ dgreed/src/font.h
@@ -0,0 +1,34 @@
+#ifndef FONT_H
+#define FONT_H
+
+#include "utils.h"
+#include "system.h"
+
+typedef uint FontHandle;
+
+// Loads a new font file
+FontHandle font_load(const char* filename); 
+// Loads a new font file, allows to provide glyph scaling factor
+FontHandle font_load_ex(const char* filename, float scale);
+// Loads a new font file, allows to specify scale and texture path prefix
+FontHandle font_load_exp(const char* filename, float scale, const char* prefix);
+// Frees font which was loaded with font_load
+void font_free(FontHandle font);
+// Returns width of text in specified font
+float font_width(FontHandle font, const char* string);
+// Returns height of font characters in pixels
+float font_height(FontHandle font);
+
+// Draws string with specified font
+void font_draw(FontHandle font, const char* string, uint layer,
+	const Vector2* topleft, Color tint);
+
+// Returns rectangle bounding drawn text.
+RectF font_rect_ex(FontHandle font, const char* string,
+	const Vector2* center, float scale);
+
+// Draws centered, scaled, string with specified font.
+void font_draw_ex(FontHandle font, const char* string, uint layer,
+	const Vector2* center, float scale, Color tint);
+
+#endif
---
+++ dgreed/src/malka/ml_coldet.h
@@ -0,0 +1,9 @@
+#ifndef ML_COLDET_H
+#define ML_COLDET_H
+
+#include "lua/lua.h"
+
+int malka_open_coldet(lua_State* l);
+
+#endif
+
