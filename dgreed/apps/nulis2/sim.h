2f811b02f2c9f0d763404978f1dea29580be7ac9
Andrius Mitkus <andrius.mitkus@gmail.com>
2012-01-31 18:08:09 +0200

    nulis2: decreased time and grav energy levels to 3

---
+++ dgreed/apps/nulis2/sim.h
@@ -0,0 +1,15 @@
+#ifndef SIM_H
+#define SIM_H
+
+#include <utils.h>
+#include <coldet.h>
+
+void sim_init(uint screen_width, uint screen_height, uint sim_width, uint sim_height);
+void sim_close(void);
+void sim_reset(const char* level);
+const char* sim_level(void);
+void sim_update(void);
+void sim_render(void);
+void sim_render_ex(bool skip_vignette);
+
+#endif
