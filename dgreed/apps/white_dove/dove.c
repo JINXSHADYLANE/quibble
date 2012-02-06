f5f4aa29ab89f3df22a883d3fd5b2e861e4b0996
Domas Markevicius <domas.marko@gmail.com>
2012-02-06 11:57:36 +0200

    nulis2: tut five finges

---
+++ dgreed/apps/white_dove/dove.c
@@ -0,0 +1,8 @@
+#include <malka/malka.h>
+
+int dgreed_main(int argc, const char** argv)
+{
+	int res = malka_run("white_dove_scripts/game.lua");
+	return res;
+}
+
---
+++ dgreed/src/DGreedAppDelegate.h
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+#import "GLESView.h"
+#import "GLESViewController.h"
+#import "AutoRotateViewController.h"
+
+@interface DGreedAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow* window;
+    AutoRotateViewController* controller;
+    GLESViewController* gl_controller;
+	GLESView* gl_view;
+}
+
+@end
+
