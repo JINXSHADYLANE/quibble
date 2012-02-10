162631cab2efd0b1d8b070c49ba5607746b20a64
Andrius Mitkus <andrius.mitkus@gmail.com>
2012-02-10 15:13:53 +0200

    spurga: tor goes back to a clean slate

---
+++ dgreed/apps/spurga/spurga_scripts/game.lua
@@ -0,0 +1,23 @@
+local game = {}
+
+function game.init()
+end
+
+function game.close()
+end
+
+function game.enter()
+end
+
+function game.leave()
+end
+
+function game.update()
+	return true
+end
+
+function game.render(t)
+	return true
+end
+
+return game
