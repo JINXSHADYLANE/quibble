054b50bae42dba46be1ce2e6e69b44c352ad10c8
Andrius Mitkus <andrius.mitkus@gmail.com>
2012-02-16 13:18:02 +0200

    spurga: title texts, slicer tool wrapper

--- dgreed/apps/spurga/spurga_scripts/hud.lua
+++ dgreed/apps/spurga/spurga_scripts/hud.lua
@@ -2,6 +2,8 @@ local hud = {}
 
 local hud_layer = 14
 local paper_layer = 15
+local text_color = rgba(0.270, 0.270, 0.270, 1)
+local title_crossfade_len = 0.5
 
 function hud.init()
 	hud.paper_spr = sprsheet.get_handle('paper')
@@ -9,9 +11,18 @@ function hud.init()
 	hud.shadow = sprsheet.get_handle('m_shadow')
 
 	video.set_blendmode(paper_layer, 'multiply')
+
+	hud.title = ''
+	hud.last_title = ''
 end
 
 function hud.close()
+end
+
+function hud.set_title(str)
+	hud.last_title = hud.title
+	hud.title = str
+	hud.title_t = time.s()
 end
 
 function hud.render()
@@ -28,6 +39,20 @@ function hud.render()
 
 	-- top shadow
 	sprsheet.draw(hud.shadow, hud_layer, rect(0, 32 + 10, scr_size.x, 32))
+
+	local t = time.s()
+
+	-- title
+	if hud.title_t and t - hud.title_t < title_crossfade_len then
+		local tt = clamp(0, 1, (t - hud.title_t) / title_crossfade_len)
+		text_color.a = tt 
+		video.draw_text_centered(fnt, hud_layer, hud.title, vec2(scr_size.x / 2, 24), 1.0, text_color)
+		text_color.a = 1 - tt
+		video.draw_text_centered(fnt, hud_layer, hud.last_title, vec2(scr_size.x / 2, 24), 1.0, text_color)
+		text_color.a = 1
+	else	
+		video.draw_text_centered(fnt, hud_layer, hud.title, vec2(scr_size.x / 2, 24), 1.0, text_color)
+	end
 end
 
 return hud
