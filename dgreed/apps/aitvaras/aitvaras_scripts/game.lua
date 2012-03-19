1d64975e15343ce20b4f6a347f8f84a1017c5951
Andrius Mitkus <andrius.mitkus@gmail.com>
2012-03-19 15:54:47 +0200

    aitvaras: some progress on game server

--- dgreed/apps/aitvaras/aitvaras_scripts/game.lua
+++ dgreed/apps/aitvaras/aitvaras_scripts/game.lua
@@ -1,11 +1,50 @@
 local game = {}
 
+tiles_w = 16
+tiles_h = 12
+
+local function map2world(p)
+	return p * 64 + vec2(32, 32)
+end
+
+-- player
+
+local player = {}
+player.__index = player
+
+function player:new(id)
+	local obj = {}
+	setmetatable(obj, self)
+	obj:randomize_pos()
+	return obj
+end
+
+function player:randomize_pos()
+	local x, y
+	
+	repeat
+		x, y = rand.int(0, tiles_w+1), rand.int(0, tiles_h+1)
+	until not tilemap.collide(game.map, map2world(vec2(x, y)))
+
+	self.pos = vec2(x, y)
+end
+
+function player:draw()
+	sprsheet.draw_anim_centered('bug_red', 0, 4, map2world(self.pos))
+end
+
+local players = {}
+
+
 function game.join(id)
 	print('player id '..tostring(id)..' joined')
+	players[id] = player:new(id)
+	print(tostring(players[id].pos))
 end
 
 function game.leave(id)
 	print('player id '..tostring(id)..' left')
+	players[id] = nil
 end
 
 function game.control(id, msg)
@@ -22,9 +61,14 @@ function game.init()
 	aitvaras.input_cb = game.control
 
 	aitvaras.init()
+
+	game.map = tilemap.load(pre..'level1.btm')
+	tilemap.set_camera(game.map, scr_size / 2, 1, 0)
 end
 
 function game.close()
+	tilemap.free(game.map)
+
 	aitvaras.close()
 end
 
@@ -39,7 +83,15 @@ function game.update()
 end
 
 function game.render(t)
-	video.draw_text(fnt, 1, tostring(aitvaras.id()), vec2(10, 690), rgba(1, 1, 1, 1))
+	video.draw_text(fnt, 15, tostring(aitvaras.id()), vec2(10, 690), rgba(1, 1, 1, 1))
+
+	tilemap.render(game.map, rect(0, 0, scr_size.x, scr_size.y))
+
+	for id,player in pairs(players) do
+		if player then
+			player:draw()
+		end
+	end
 
 	return true
 end
