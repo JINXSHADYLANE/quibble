e93962a70dfe1865061693df6b9df668a860eae7
Domas Markevicius <domas.marko@gmail.com>
2012-03-26 19:25:56 +0300

    aitvaras: web controls changed

---
+++ dgreed/src/AutoRotateViewController.h
@@ -0,0 +1,14 @@
+//
+//  AutoRotateViewController.h
+//  nulis
+//
+//  Created by Andrius Mitkus on 2/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GameKit/GameKit.h>
+
+@interface AutoRotateViewController : UIViewController 
+
+@end
