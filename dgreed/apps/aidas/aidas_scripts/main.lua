ee89d7dfdc4ca194c9c8da6f2b59375aa2d5840b
Andrius Mitkus <andrius.mitkus@gmail.com>
2012-04-21 12:04:26 +0300

    aidas: my LD23 game

---
+++ dgreed/apps/aidas/aidas_scripts/main.lua
@@ -0,0 +1,36 @@
+
+pre = 'aidas_assets/'
+
+scr_size = vec2(480, 320)
+
+local game = require('game')
+
+function game_init()
+	local scale = 2
+	local real_size = scr_size * scale
+	video.init_exr(real_size.x, real_size.y, scr_size.x, scr_size.y, 'aidas', false)
+	sound.init()
+
+	--mfx.init(pre..'effects.mml')
+	--sprsheet.init(pre..'sprsheet.mml')
+	--mus = sound.load_stream(pre..'music.ogg')
+	--sound.set_volume(mus, 0.7)
+	--sound.play(mus, true)
+
+	--fnt = font.load(pre..'lucida_grande_30px.bft', 0.5)
+
+	--particles.init('aidas_assets/', 6)
+
+	states.register('game', game)
+	states.push('game')
+end
+
+function game_close()
+	--particles.close()
+	--font.free(fnt)
+	--sound.free(mus)
+	--sprsheet.close()
+	--mfx.close()
+	sound.close()
+	video.close()
+end
