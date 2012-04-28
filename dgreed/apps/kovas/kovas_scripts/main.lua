5e20209ef0504d7d7fb7e77741df03ebbc33cd02
Andrius Mitkus <andrius.mitkus@gmail.com>
2012-04-28 14:33:11 +0300

    kovas: new secret project

---
+++ dgreed/apps/kovas/kovas_scripts/main.lua
@@ -0,0 +1,31 @@
+
+pre = 'kovas_assets/'
+
+scr_size = vec2(1024, 768)
+screen_rect = rect(0, 0, scr_size.x, scr_size.y)
+
+local game = require('game')
+
+function game_init()
+	video.init(scr_size.x, scr_size.y, 'aidas')
+	sound.init()
+
+--	mfx.init(pre..'effects.mml')
+--	sprsheet.init(pre..'sprsheet.mml')
+--	mus = sound.load_stream(pre..'aidas.ogg')
+--	sound.play(mus, true)
+
+--	fnt = font.load(pre..'gentium_60px.bft', 0.5, pre)
+
+	states.register('game', game)
+	states.push('game')
+end
+
+function game_close()
+--	font.free(fnt)
+--	sound.free(mus)
+--	sprsheet.close()
+--	mfx.close()
+	sound.close()
+	video.close()
+end
