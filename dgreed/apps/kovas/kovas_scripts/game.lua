168f02b05613ae6a8faa455ea0b70c5ebb3d8a61
Andrius Mitkus <andrius.mitkus@gmail.com>
2012-04-28 15:35:00 +0300

    kovas: scrolling, trees

--- dgreed/apps/kovas/kovas_scripts/game.lua
+++ dgreed/apps/kovas/kovas_scripts/game.lua
@@ -5,9 +5,14 @@ local game = {}
 function game.init()
 	game.map = hexgrid:new()
 
-	game.map:set(0, 0, 1)
-	game.map:set(-1, 0, 1)
-	game.map:set(0, 1, 1)
+	game.camera = vec2(0, 0)
+
+	-- plant 100000 trees
+	for i=1,100000 do
+		local x = rand.int(-500, 500)
+		local y = rand.int(-500, 500)
+		game.map:set(x, y, 'tree')
+	end
 
 end
 
@@ -24,11 +29,28 @@ function game.update()
 	if key.down(key.quit) then
 		states.pop()
 	end
+
+	if key.pressed(key._up) then
+		game.camera.y = game.camera.y - 2
+	end
+
+	if key.pressed(key._down) then
+		game.camera.y = game.camera.y + 2
+	end
+	
+	if key.pressed(key._left) then
+		game.camera.x = game.camera.x - 2
+	end
+
+	if key.pressed(key._right) then
+		game.camera.x = game.camera.x + 2
+	end
+
 	return true
 end
 
 function game.render(t)
-	game.map:draw(vec2(0, 0), rect(0, 0, scr_size.x, scr_size.y))
+	game.map:draw(game.camera, rect(0, 0, scr_size.x, scr_size.y))
 
 	return true
 end
