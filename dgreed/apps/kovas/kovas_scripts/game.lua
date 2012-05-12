c88b76216ba245f9dad9958213933de0fe20cedf
Andrius Mitkus <andrius.mitkus@gmail.com>
2012-05-12 15:41:48 +0300

    kovas: player against wall sliding collisions

--- dgreed/apps/kovas/kovas_scripts/game.lua
+++ dgreed/apps/kovas/kovas_scripts/game.lua
@@ -1,9 +1,11 @@
 local hexgrid = require('hexgrid')
+local player = require('player')
 
 local game = {}
 
 function game.init()
 	game.map = hexgrid:new()
+	game.player = player:new()
 
 	game.camera = vec2(0, 0)
 
@@ -30,27 +32,16 @@ function game.update()
 		states.pop()
 	end
 
-	if key.pressed(key._up) then
-		game.camera.y = game.camera.y - 2
-	end
-
-	if key.pressed(key._down) then
-		game.camera.y = game.camera.y + 2
-	end
-	
-	if key.pressed(key._left) then
-		game.camera.x = game.camera.x - 2
-	end
-
-	if key.pressed(key._right) then
-		game.camera.x = game.camera.x + 2
-	end
+	game.player:update(game.map)
+	game.camera = lerp(game.camera, game.player.p, 0.1)
 
 	return true
 end
 
 function game.render(t)
-	game.map:draw(game.camera, rect(0, 0, scr_size.x, scr_size.y))
+	local screen = rect(0, 0, scr_size.x, scr_size.y)
+	game.map:draw(game.camera, screen)
+	game.player:draw(game.camera, screen)
 
 	return true
 end
