0af0864cb3a73881cc2525f66c952bf6acc36481
Justina Mickonyt? <j.mickonyte@gmail.com>
2012-10-27 17:06:31 +0300

    bailys: slightly more exact laser

---
+++ dgreed/apps/bailys/bailys_scripts/levels.lua
@@ -0,0 +1,43 @@
+local levels = {}
+
+levels[1] = {
+	'      ',
+	'      ',
+	'  ##eS',
+	'    e ',
+	'       '
+}
+
+levels[2] = {
+	'         ',
+	'  ##     ',
+	' r    l S',
+	'    ee l ',
+	'        l'
+}
+
+levels[3] = {
+	'      ',
+	'  e   ',
+	'S  e  ',
+	'  e   ',
+	'      '
+ }
+
+levels[4] = {
+	'     #S',
+	'll r   ',
+	'r   eee',
+	'       '
+}
+
+levels[5] = {
+	'  #e eee',
+	' e# #ee ',
+	' ## ###e',
+	'   e#S  ',
+	'    #   ',
+}
+
+return levels
+
