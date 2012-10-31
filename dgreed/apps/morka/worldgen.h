3b58f5019a6ab030e46b86048ada498fbd58dc9e
Andrius Mitkus <andrius.mitkus@gmail.com>
2012-10-31 12:48:43 +0200

    morka: heavily simplified worldgen

--- dgreed/apps/morka/worldgen.h
+++ dgreed/apps/morka/worldgen.h
@@ -1,30 +1,12 @@
 #ifndef WORLDGEN_H
 #define WORLDGEN_H
 
-#include <datastruct.h>
-#include "objects.h"
-
-typedef struct {
-	GameObjectDesc* desc;
-	Vector2 pos;
-	void* userdata;
-	ListHead list;
-} WorldElement;
-
-typedef struct {
-	ListHead background;
-	ListHead mushrooms;
-} WorldPage;
+#include <utils.h>
 
 void worldgen_reset(uint seed);
 void worldgen_close(void);
 
-WorldPage* worldgen_current(void);
-WorldPage* worldgen_next(void);
-
-WorldPage* worldgen_new_page(void);
-
-void worldgen_show(WorldPage* page);
+void worldgen_update(float camera_extent_max);
 
 #endif
 
