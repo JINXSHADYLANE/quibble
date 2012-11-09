bc73a86dac378a6cc6bb75bd8516e75dd3fdfd2d
Andrius Mitkus <andrius.mitkus@gmail.com>
2012-11-09 13:09:20 +0200

    dgreed: heapsort implementation

---
+++ dgreed/apps/morka/game.h
@@ -0,0 +1,10 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <malka/ml_states.h>
+
+extern StateDesc game_state;
+
+void game_reset(void);
+
+#endif
---
+++ dgreed/apps/morka/obj_types.h
@@ -0,0 +1,77 @@
+#ifndef OBJ_TYPES_H
+#define OBJ_TYPES_H
+
+#include <anim.h>
+
+#include "objects.h"
+
+enum {
+	OBJ_RABBIT_TYPE = 1,
+	OBJ_GROUND_TYPE,
+	OBJ_DECO_TYPE,
+	OBJ_MUSHROOM_TYPE,
+	OBJ_CLOCK_TYPE,
+	OBJ_CLOCK_FADING_TYPE
+};
+
+// Rabbit
+
+typedef struct {
+	GameObject header;
+	bool touching_ground;
+	bool can_double_jump;
+	bool jump_off_mushroom;
+	float jump_time;
+	float mushroom_hit_time;
+	float last_keypress_t;
+	Anim* anim;
+	Vector2 bounce_force;
+} ObjRabbit;
+
+extern GameObjectDesc obj_rabbit_desc;
+
+// Ground
+
+typedef struct {
+	GameObject header;
+} ObjGround;
+
+extern GameObjectDesc obj_ground_desc;
+
+// Deco (static image, destroys itself when not visible)
+
+typedef struct {
+	GameObject header;
+} ObjDeco;
+
+extern GameObjectDesc obj_deco_desc;
+
+// Mushroom
+
+typedef struct {
+	GameObject header;
+} ObjMushroom;
+
+extern GameObjectDesc obj_mushroom_desc;
+
+// Clock
+
+typedef struct {
+	GameObject header;
+} ObjClock;
+
+extern GameObjectDesc obj_clock_desc;
+
+// Fading clock
+
+typedef struct {
+	GameObject header;
+	float t;
+	float birth_time;
+	Vector2 original_pos;
+} ObjClockFading;
+
+extern GameObjectDesc obj_clock_fading_desc;
+
+#endif
+
