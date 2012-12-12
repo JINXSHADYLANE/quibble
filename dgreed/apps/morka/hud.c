6a81d62155ab8c724e946c6f245709be143c2224
Andrius Mitkus <andrius.mitkus@gmail.com>
2012-12-12 16:45:24 +0200

    morka: display more stats on game over

---
+++ dgreed/apps/morka/hud.c
@@ -0,0 +1,242 @@
+#include "hud.h"
+
+#include <uidesc.h>
+#include <vfont.h>
+
+#define hud_layer 7
+
+extern float rabbit_remaining_time;
+extern float rabbit_distance;
+extern float rabbit_current_distance;
+
+extern void game_reset(void);
+extern void game_pause(void);
+extern void game_unpause(void);
+extern bool game_is_paused(void);
+
+extern bool game_over;
+float game_over_alpha = 0.0f;
+
+static uint last_combo = 0;
+static uint current_combo = 0;
+static uint longest_combo = 0;
+static float combo_flip_t = 0.0f;
+
+uint clocks_collected = 0;
+
+static void _hud_render_ui(UIElement* element, uint layer) {
+	// Render
+	if(element->members & UI_EL_SPR) {
+		RectF dest = rectf_null();
+		if(element->members & UI_EL_RECT) {
+			dest = element->rect;
+		}
+		else if(element->members & UI_EL_VEC2) {
+			dest.left = element->vec2.x;
+			dest.top = element->vec2.y;
+		}
+
+		spr_draw_h(element->spr, layer, dest, COLOR_WHITE);
+	}
+
+	// Iterate over children
+	UIElement* child;
+	list_for_each_entry(child, &element->child_list, list) {
+		_hud_render_ui(child, layer-1);
+	}
+}
+
+static void _hud_render_clock_needle(UIElement* element, uint layer, float angle) {
+	spr_draw_cntr_h(element->spr, layer, element->vec2, angle, 1.0f, COLOR_WHITE);
+}
+static void _hud_render_game_over(UIElement* element, uint layer, float alpha) {
+	UIElement* text = uidesc_get_child(element, "text");
+	UIElement* dist_text = uidesc_get_child(element, "distance_text");
+	UIElement* combo_text = uidesc_get_child(element, "combo_text");
+	UIElement* clocks_text = uidesc_get_child(element, "clocks_text");
+	UIElement* button = uidesc_get_child(element, "button");
+
+	byte a = lrintf(255.0f * alpha);
+	Color col = COLOR_RGBA(255, 255, 255, a);
+
+	spr_draw("blue_shade", layer, rectf(0.0f, 0.0f, 1024.0f, 768.0f), col); 
+
+	// Text
+	vfont_select("Baskerville-Bold", 48.0f); 
+	const char* str = "The time is over.";
+	static Vector2 half_size = {0.0f, 0.0f};
+	if(half_size.x == 0.0f) {
+		half_size = vec2_scale(vfont_size(str), 0.5f);
+	}
+	vfont_draw(str, layer, vec2_sub(text->vec2, half_size), col);
+
+	// Distance text
+	char distance_str[32];
+	sprintf(distance_str, "You ran %d meters", (int)lrintf(rabbit_distance));
+	Vector2 half_dist_size = vec2_scale(vfont_size(distance_str), 0.5f);
+	vfont_draw(distance_str, layer, vec2_sub(dist_text->vec2, half_dist_size), col);
+
+	// Combo text
+	char combo_str[32];
+	sprintf(combo_str, "Longest combo - %u", longest_combo);
+	Vector2 half_combo_size = vec2_scale(vfont_size(combo_str), 0.5f);
+	vfont_draw(combo_str, layer, vec2_sub(combo_text->vec2, half_combo_size), col);
+	
+	// Clocks text
+	char clocks_str[32];
+	sprintf(clocks_str, "Clocks collected - %u", clocks_collected);
+	Vector2 half_clocks_size = vec2_scale(vfont_size(clocks_str), 0.5f);
+	vfont_draw(clocks_str, layer, vec2_sub(clocks_text->vec2, half_clocks_size), col);
+
+	// Button
+	spr_draw_cntr_h(button->spr, layer, button->vec2, 0.0f, 1.0f, col);	
+
+	if(touches_count() > 0) {
+		Touch* t = touches_get();
+		if(vec2_length_sq(vec2_sub(t[0].hit_pos, button->vec2)) < 40.0f * 40.0f) {
+			game_reset();
+		}
+	}
+}
+
+static void _hud_render_pause(UIElement* element, uint layer) {
+	UIElement* text = uidesc_get_child(element, "text");
+	UIElement* button = uidesc_get_child(element, "button");
+
+	spr_draw("blue_shade", layer, rectf(0.0f, 0.0f, 1024.0f, 768.0f), COLOR_WHITE); 
+
+	// Text
+	vfont_select("Baskerville-Bold", 48.0f); 
+	const char* str = "The game is paused.";
+	static Vector2 half_size = {0.0f, 0.0f};
+	if(half_size.x == 0.0f) {
+		half_size = vec2_scale(vfont_size(str), 0.5f);
+	}
+	vfont_draw(str, layer, vec2_sub(text->vec2, half_size), COLOR_WHITE);
+
+	// TODO: Button
+	spr_draw_cntr_h(button->spr, layer, button->vec2, 0.0f, 1.0f, COLOR_WHITE);
+
+	if(touches_count() > 0) {
+		Touch* t = touches_get();
+		if(vec2_length_sq(vec2_sub(t[0].hit_pos, button->vec2)) < 40.0f * 40.0f) {
+			game_unpause();
+			time_scale(1.0f);
+		}
+	}
+}
+
+static void _hud_render_distance(UIElement* element, uint layer, uint number) {
+	uint digits[4];
+	for(uint i = 0; i < 4; ++i) {
+		digits[3-i] = number % 10;
+		number /= 10;
+	}
+
+	vfont_select("Baskerville-Bold", 18.0f);
+
+	UIElement* digit1 = uidesc_get_child(element, "digit1");
+	UIElement* digit_offset = uidesc_get_child(element, "digit_offset");
+	Vector2 cursor = digit1->vec2;
+	char str[] = {'\0', '\0'};
+	for(uint i = 0; i < 4; ++i) {
+		str[0] = '0' + digits[i];
+		vfont_draw(str, layer, cursor, COLOR_BLACK);
+		cursor = vec2_add(cursor, digit_offset->vec2);
+		cursor.x = floorf(cursor.x);
+	}
+}
+
+static void _hud_render_combo(UIElement* element, uint layer, uint mult, float t) {
+	vfont_select("Baskerville-Bold", 48.0f);
+
+	// Combine two smootherstep functions into a new one,
+	// where x stays constant for a while near t = 0.5
+	float x;
+	if(t < 0.5f)
+		x = smootherstep(-1.0f, 0.0f, t * 2.0f);
+	else
+		x = smootherstep(0.0f, 1.0f, (t - 0.5f) * 2.0f);
+
+	// Classic sine there-and-back-again for alpha
+	float a = MAX(0.0f, sinf((t*1.4f - 0.2f) * PI));
+
+	char text[64];
+	sprintf(text, "Combo x%u", mult);
+	Vector2 half_size = vec2_scale(vfont_size(text), 0.5f);
+
+	Vector2 pos = vec2_sub(element->vec2, half_size);
+	pos.x -= x * 200.0f;
+	vfont_draw(text, layer, pos, COLOR_FTRANSP(a));
+}
+
+void hud_init(void) {
+	vfont_init();
+}
+
+void hud_close(void) {
+	vfont_close();
+}
+
+void hud_trigger_combo(uint multiplier) {
+	last_combo = current_combo;
+	current_combo = multiplier;
+	longest_combo = MAX(longest_combo, current_combo);
+	combo_flip_t = time_s();
+}
+
+void hud_render(void) {
+	UIElement* pause = uidesc_get("hud_pause");
+	_hud_render_ui(pause, hud_layer);
+	if(touches_count() > 0) {
+		Touch* t = touches_get();
+		if(vec2_length_sq(vec2_sub(t[0].hit_pos, pause->vec2)) < 40.0f * 40.0f) {
+			time_scale(0.0f);
+			game_pause();
+		}
+	}
+
+	game_over_alpha += game_over ? 0.03f : -0.05f;
+	game_over_alpha = clamp(0.0f, 1.0f, game_over_alpha);
+
+	static bool showed_game_over_last_frame = false;
+	if(game_over_alpha > 0.0f) {
+		showed_game_over_last_frame = true;
+		_hud_render_game_over(uidesc_get("game_over"), hud_layer+1, game_over_alpha);
+	}
+	else {
+		if(showed_game_over_last_frame) {
+			showed_game_over_last_frame = false;
+			longest_combo = 0;
+			clocks_collected = 0;
+		}
+		if(game_is_paused()) {
+			UIElement* pause_screen = uidesc_get("pause");
+			_hud_render_pause(pause_screen, hud_layer+1);
+		}
+	}
+
+	UIElement* clock = uidesc_get("hud_clock");
+	_hud_render_ui(clock, hud_layer);
+	_hud_render_distance(clock, hud_layer+1, lrintf(rabbit_current_distance));
+
+	UIElement* hud_clock_needle = uidesc_get("hud_clock_needle");
+
+	float angle = (rabbit_remaining_time / 60.0f) * 2.0f * PI;
+	_hud_render_clock_needle(hud_clock_needle, hud_layer, angle);
+
+	UIElement* combo_text = uidesc_get("combo_text");
+	float ts = time_s();
+	float t = (ts - combo_flip_t) / 0.4f;
+	if(t < 1.0f) {
+		if(current_combo)
+			_hud_render_combo(combo_text, hud_layer+1, current_combo, t * 0.5f);
+		if(last_combo)
+			_hud_render_combo(combo_text, hud_layer+1, last_combo, 0.5f + t * 0.5f);
+	}
+	else {
+		if(current_combo)
+			_hud_render_combo(combo_text, hud_layer+1, current_combo, 0.5f);
+	}
+}
+
