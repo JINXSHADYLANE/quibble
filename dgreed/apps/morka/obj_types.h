52e34a67cc3c1dc220aab3c336069ea924ee7ad1
Tadas <tadbask@gmail.com>
2013-01-25 11:35:23 +0200

    morka: fixed rabbit animation, cleanup

--- dgreed/apps/morka/obj_types.h
+++ dgreed/apps/morka/obj_types.h
@@ -21,8 +21,7 @@ typedef struct {
 	bool touching_ground;
 	bool jump_off_mushroom;
 	bool is_diving;
-	bool is_oob;
-	float speed_adjust;
+	bool is_dead;
 	Anim* anim;
 	Vector2 bounce_force;
 } ObjRabbit;
@@ -33,7 +32,6 @@ extern GameObjectDesc obj_rabbit_desc;
 
 typedef struct {
 	GameObject header;
-	float speed_adjust;
 } ObjGround;
 
 extern GameObjectDesc obj_ground_desc;
