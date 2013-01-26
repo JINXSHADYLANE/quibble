1475f097c8d483bd115e692210628e7d9ea09bf0
Andrius Mitkus <andrius.mitkus@gmail.com>
2013-01-26 23:29:18 +0200

    gija: doors

--- dgreed/apps/gija/gija_scripts/game.lua
+++ dgreed/apps/gija/gija_scripts/game.lua
@@ -14,6 +14,7 @@ local camera = {
 
 local char0 = nil
 local char1 = nil
+local doors = {} 
 
 local obj_type = {
 	start0 = 0,
@@ -23,7 +24,7 @@ local obj_type = {
 	door1 = 4
 }
 
-function sweep_rect(r, offset)
+function sweep_rect(r, offset, i, w)
 	local off, d
 
 	-- collide against level
@@ -33,6 +34,20 @@ function sweep_rect(r, offset)
 	else
 		off = tilemap.collide_swept(tmap1, r, offset)
 		d = length_sq(off)
+	end
+
+	-- collide against doors
+	if d > 0 then
+		for _,dr in ipairs(doors) do
+			if dr.color ~= i and dr.world == w then
+				local door_off = rect_rect_sweep(dr.rect, r, offset)
+				local door_d = length_sq(door_off)
+				if d - door_d > 0.001 then
+					d = door_d
+					off = door_off
+				end
+			end
+		end
 	end
 
 	return off
@@ -46,18 +61,34 @@ function game.reset(level_name)
 	tmap0 = nil
 	tmap1 = nil
 	objs = nil
+	doors = {}
 
 	if level_name == nil then
-		level_name = 'level2'
+		level_name = 'level3'
 	end
 
 	tmap0 = tilemap.load(pre..level_name..'.0.btm')
 	tmap1 = tilemap.load(pre..level_name..'.1.btm')
 	objs = tilemap.objects(tmap0)
 
+	local add_door = function(w, obj)
+		local r = rect(
+			obj.pos.x, obj.pos.y,
+			obj.pos.x + 32, obj.pos.y + 64
+		)
+		local c = 1
+		if obj.id == obj_type.door1 then
+			c = 2
+		end
+		table.insert(doors, {
+			rect = r,
+			color = c,
+			world = w
+		})
+	end
+
 	-- set up objects
 	for i,obj in ipairs(objs) do
-		print(obj.id, obj.pos)
 		if obj.id == obj_type.start0 then
 			char0 = character:new(obj, 1)
 			camera.center = vec2(obj.pos)
@@ -65,11 +96,20 @@ function game.reset(level_name)
 		if obj.id == obj_type.start1 then
 			char1 = character:new(obj, 2)
 		end
+		if obj.id == obj_type.door0 or obj.id == obj_type.door1 then
+			add_door(0, obj)	
+		end
+	end
+
+	objs = tilemap.objects(tmap1)
+	for i,obj in ipairs(objs) do
+		if obj.id == obj_type.door0 or obj.id == obj_type.door1 then
+			add_door(1, obj)
+		end
 	end
 end
 
 function game.death()
-	print('reset level')
 	game.reset()
 end
 
@@ -123,17 +163,37 @@ function game.update()
 
 	game.update_camera()
 
+	local w = 0
+	if char1.heart then
+		w = 1
+	end
+
 	assert(char0 and char1)
-	char0:update(sweep_rect, game.switch_hearts)
-	char1:update(sweep_rect, game.switch_hearts)
+	char0:update(sweep_rect, game.switch_hearts, w)
+	char1:update(sweep_rect, game.switch_hearts, w)
 
 	if char0.bbox and char1.bbox then
-		if char0.bbox.b >= 576 * 3 or char1.bbox.b >= 576 * 3 then
+		if char0.bbox.b >= 575 * 3 or char1.bbox.b >= 575 * 3 then
 			game.death()
 		end
 	end
 
 	return not key.down(key.quit)
+end
+
+function game.render_doors(map, w)
+	for _,dr in pairs(doors) do
+		if dr.world == w then
+			local pos = tilemap.world2screen(map, scr_rect, dr.rect)
+			local spr = 'char1'
+			if dr.color == 2 then
+				spr = 'char2'
+			end
+			if rect_rect_collision(pos, scr_rect) then
+				sprsheet.draw(spr, 3, pos)
+			end
+		end
+	end
 end
 
 function game.render(t)
@@ -143,10 +203,12 @@ function game.render(t)
 
 	if char0.heart and tmap0 then
 		tilemap.render(tmap0, scr_rect)
+		game.render_doors(tmap0, 0)
 	end
 	
 	if char1.heart and tmap1 then
 		tilemap.render(tmap1, scr_rect)
+		game.render_doors(tmap1, 1)
 	end
 
 	if char0 then
