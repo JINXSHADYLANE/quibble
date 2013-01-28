7eadb070b5f0dec1eb5df7fa4bdb36cfa93e3373
Tadas <tadbask@gmail.com>
2013-01-28 15:59:33 +0200

    morka: second mushroom fix, combo tweak

--- dgreed/apps/morka/worldgen.h
+++ dgreed/apps/morka/worldgen.h
@@ -7,6 +7,7 @@ void worldgen_reset(uint seed);
 void worldgen_close(void);
 
 void worldgen_update(float fg_camera_extent_max, float bg_camera_extent_max);
+void worldgen_debug_render();
 
 #endif
 
