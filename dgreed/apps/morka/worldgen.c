3a97bf3123c0d9ade99ee91a05b060b9ab8c964d
Tadas <tadbask@gmail.com>
2013-01-28 13:03:08 +0200

    morka: flying mushroom fix

--- dgreed/apps/morka/worldgen.c
+++ dgreed/apps/morka/worldgen.c
@@ -26,6 +26,7 @@ static void _gen_bg_page(void) {
 	while(bg_x < page_width) {
 		char sym = mchains_next(bg_chain, &rnd);
 		mchains_symbol_info(bg_chain, sym, &advance, &spr);
+		mchains_symbol_info(bg_chain, sym, &advance, &spr);
 
 		if(spr) {
 			Vector2 pos = vec2(bg_page_cursor + bg_x + 100.0f, 680.0f);
@@ -38,12 +39,17 @@ static void _gen_bg_page(void) {
 	bg_page_cursor += page_width;
 }
 
-
-
 static void _gen_fg_page(void) {
 	SprHandle spr;	
 	uint advance = 0;
-	uint prev_advance = 0;
+	static uint prev_advance = 0;
+	
+	static Vector2 gaps[4];
+	static const int max_gaps = 4;
+	static int gaps_i = 0;
+	static int previuos_gaps = 0;
+	
+	gaps_i = previuos_gaps;
 	
 	// Add ground
 	static float ground_x = page_width;
@@ -53,25 +59,38 @@ static void _gen_fg_page(void) {
 		mchains_symbol_info(ground_chain, sym, &advance, &spr);
 		if(spr) {
 			Vector2 pos = vec2(fg_page_cursor + ground_x + 100.0f, 768.0f);
+			advance = (uint) sprsheet_get_size_h(spr).x;
 			if(sym == 'a' || sym == 'h'){	// no collision for grass_start1 and grass_end2
 				objects_create(&obj_fg_deco_desc, pos, (void*)spr);
+				prev_advance = advance;	
 			} else {
 				objects_create(&obj_ground_desc, pos, (void*)spr);
 				if(sym == 'j' || sym == 'k' || sym == 'l' || sym == 'm' || sym == 'n' || sym == 'o'){
 					ObjSpeedTrigger* t = (ObjSpeedTrigger*)objects_create(&obj_speed_trigger_desc, pos, (void*)spr);
 					t->drag_coef = 0.9;
 				}
-			}
-			advance = (uint) sprsheet_get_size_h(spr).x;
-			prev_advance = advance;			
+				prev_advance = 0;
+			}		
 		} else {
 			if(sym == '_' || sym == '-' || sym == '='){
 				Vector2 pos = vec2(fg_page_cursor + ground_x + 100.0f - prev_advance, 768.0f);
+				
+				// add gap to gap array
+				gaps[gaps_i].x = pos.x;									// start of gap
+				gaps[gaps_i].y = pos.x + advance + 120 + 50;			// end of gap
+
+				if(gaps_i > max_gaps){ 
+					gaps_i = 0;
+					printf("gaps_i > max_gaps !\n");
+				}
+				
+				gaps_i++;
 				objects_create(&obj_fall_trigger_desc, pos, (void*)advance);
 			}
 		}
 		ground_x += (float)advance;
 	}
+	previuos_gaps = gaps_i - previuos_gaps;
 	
 	// Add foreground mushrooms
 	static float fg_x = page_width;
@@ -79,9 +98,20 @@ static void _gen_fg_page(void) {
 	while(fg_x < page_width) {
 		char sym = mchains_next(fg_chain, &rnd);
 		mchains_symbol_info(fg_chain, sym, &advance, &spr);
-
-		if(spr) {
-			Vector2 pos = vec2(fg_page_cursor + fg_x + 100.0f, 641.0f);
+		Vector2 pos = vec2(fg_page_cursor + fg_x + 100.0f, 641.0f);
+		
+		bool place = false;
+		
+		if(spr){
+			place = true;
+			for(int i = 0; i < gaps_i;i++){
+				if( (pos.x > gaps[i].x && pos.x < gaps[i].y) || (pos.x+advance > gaps[i].x && pos.x+advance < gaps[i].y) || (pos.x < gaps[i].x && pos.x+advance > gaps[i].y )){
+					place = false;
+				}
+			}
+		}
+				
+		if(place) {
 			GameObject* g = objects_create(&obj_mushroom_desc, pos, (void*)spr);
 			ObjMushroom* shroom = (ObjMushroom*)g;
 			if(sym == 'x')
@@ -95,6 +125,11 @@ static void _gen_fg_page(void) {
 	}
 
 	fg_page_cursor += page_width;
+	
+	for(int i = 0; i < gaps_i - previuos_gaps; i++){
+		gaps[i].x = gaps[i+1].x;
+		gaps[i].y = gaps[i+1].y; 		
+	}		
 }
 void worldgen_reset(uint seed) {
 	if(!rnd) {
