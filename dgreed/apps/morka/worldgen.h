bb7b4422bf258eddde8be71bf7edad3aba69b422
Andrius Mitkus <andrius.mitkus@gmail.com>
2013-01-31 13:06:58 +0200

    morka: levels subsystem design

--- dgreed/apps/morka/worldgen.h
+++ dgreed/apps/morka/worldgen.h
@@ -3,7 +3,9 @@
 
 #include <utils.h>
 
-void worldgen_reset(uint seed);
+#include "levels.h"
+
+void worldgen_reset(uint seed, const LevelDesc* desc);
 void worldgen_close(void);
 
 void worldgen_update(float fg_camera_extent_max, float bg_camera_extent_max);
