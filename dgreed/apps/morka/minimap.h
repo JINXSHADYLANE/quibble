8568ef3c1a153df6122163387e238cc98f90f4b5
Tadas <tadbask@gmail.com>
2013-02-14 14:57:45 +0200

    morka: added tokens, cactus fix

---
+++ dgreed/apps/morka/minimap.h
@@ -0,0 +1,26 @@
+#ifndef MINIMAP_H
+#define MINIMAP_H
+
+#include "obj_types.h"
+#include <darray.h>
+
+void minimap_init();
+void minimap_close(void);
+
+void minimap_track(ObjRabbit* rabbit);
+
+void minimap_draw();
+
+void minimap_reset(uint distance);
+
+float minimap_max_x();
+float minimap_min_x();
+
+float minimap_player_x();
+
+void minimap_update_places();
+
+uint minimap_get_count();
+ObjRabbit* minimap_get_place(uint i);
+
+#endif
---
+++ dgreed/src/system.h
@@ -0,0 +1,12 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include "utils.h"
+
+extern float x_size_factor, y_size_factor;
+
+#include "sys_gfx.h"
+#include "sys_snd.h"
+#include "sys_misc.h"
+
+#endif
