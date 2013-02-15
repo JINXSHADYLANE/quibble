3f7a8216035f8f2ce4e005a1b0786615487e1aa0
Domas Markevicius <domas.marko@gmail.com>
2013-02-15 17:49:06 +0200

    morka: mchains update

---
+++ dgreed/apps/morka/obj_mushroom.c
@@ -0,0 +1,73 @@
+#include "obj_types.h"
+#include "common.h"
+#include <system.h>
+
+static void obj_mushroom_collide(GameObject* self, GameObject* other) {
+	ObjMushroom* mushroom = (ObjMushroom*)self;
+	if(other->type == OBJ_RABBIT_TYPE) {
+		PhysicsComponent* rabbit_physics = other->physics;
+		if(rabbit_physics->vel.y > 0.0f) {
+			mushroom->dh -= 10.0f;
+		}
+	}
+}
+
+static void obj_mushroom_update_pos(GameObject* self) {
+	ObjMushroom* mushroom = (ObjMushroom*)self;
+	RenderComponent* r = self->render;
+
+	float f = 30.0f * (mushroom->oh - mushroom->h);
+	mushroom->dh += f * (time_delta()/1000.0f);
+	mushroom->h += (mushroom->dh * 10.0f) * (time_delta()/1000.0f);
+	mushroom->dh *= 0.9f;
+
+	r->world_dest.top = r->world_dest.bottom - mushroom->h;
+}
+
+static void obj_mushroom_construct(GameObject* self, Vector2 pos, void* user_data) {
+	SprHandle spr_handle = (SprHandle)user_data;
+
+	ObjMushroom* mushroom = (ObjMushroom*)self;
+
+	Vector2 size = sprsheet_get_size_h(spr_handle);
+	float width = size.x;
+	float height = size.y;
+
+	mushroom->oh = height;
+	mushroom->h = height;
+	mushroom->dh = 0.0f;
+
+	// Take 10 pixel slice at the top as a collider geometry
+	RectF collider = {
+		pos.x  + 20.0f, 		pos.y - height,
+		pos.x  + width - 20.0f, pos.y - height + 10.0f
+	};
+
+	// Physics
+	PhysicsComponent* physics = self->physics;
+	physics->cd_obj = coldet_new_aabb(objects_cdworld, &collider, 1, NULL);
+	physics->inv_mass = 0.0f;
+	physics->hit_callback = obj_mushroom_collide;
+
+	// Render
+	RenderComponent* render = self->render;
+	render->world_dest = rectf(
+			pos.x, pos.y - height,
+			pos.x + width, pos.y
+	);
+	render->layer = foreground_layer;
+	render->anim_frame = MAX_UINT16;
+	render->spr = spr_handle;
+	render->update_pos = obj_mushroom_update_pos;
+}
+
+GameObjectDesc obj_mushroom_desc = {
+	.type = OBJ_MUSHROOM_TYPE,
+	.size = sizeof(ObjMushroom),
+	.has_physics = true,
+	.has_render = true,
+	.has_update = false,
+	.construct = obj_mushroom_construct,
+	.destruct = NULL
+};
+
---
+++ dgreed/src/gui.h
@@ -0,0 +1,59 @@
+#ifndef GUI_H
+#define GUI_H
+
+#include "system.h"
+#include "font.h"
+
+typedef struct {
+	TexHandle texture;
+	FontHandle font;
+
+	Color text_color;
+
+	uint first_layer;
+	uint second_layer;
+	uint text_layer;
+
+	RectF src_button_down;
+	RectF src_button_up;
+
+	RectF src_switch_on_up;
+	RectF src_switch_on_down;
+	RectF src_switch_off_up;
+	RectF src_switch_off_down;
+
+	RectF src_slider;
+	RectF src_slider_knob_down;
+	RectF src_slider_knob_up;
+} GuiDesc;
+
+GuiDesc gui_default_style(const char* assets_prefix);
+bool gui_validate_desc(const GuiDesc* desc);
+
+void gui_init(const GuiDesc* desc);
+void gui_close(void);
+
+// Draws interactive widgets, returns their state 
+void gui_label(const Vector2* pos, const char* text);
+bool gui_button(const Vector2* pos, const char* text);
+bool gui_switch(const Vector2* pos, const char* text);
+float gui_slider(const Vector2* pos);
+
+// Widget state access
+bool gui_getstate_switch(const Vector2* pos);
+float gui_getstate_slider(const Vector2* pos);
+void gui_setstate_switch(const Vector2* pos, bool val);
+void gui_setstate_slider(const Vector2* pos, float val);
+
+// Draws inactive gui elements, useful for transitions/animations
+void gui_draw_label(const Vector2* center, float scale,
+	const char* text, Color tint);
+void gui_draw_button(const Vector2* center, float scale, 
+	const char* text, bool state, Color tint);
+void gui_draw_switch(const Vector2* center, float scale,
+	const char* text, bool state, Color tint);
+void gui_draw_slider(const Vector2* center, float scale,
+	float state, Color tint);
+	
+
+#endif
