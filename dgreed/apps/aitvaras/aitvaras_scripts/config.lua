16e99f879de7fdbb14a0ea791d3c986ba8d21bd9
Andrius Mitkus <andrius.mitkus@gmail.com>
2013-02-22 18:48:43 +0200

    morka: retina ipad support

---
+++ dgreed/apps/aitvaras/aitvaras_scripts/config.lua
@@ -0,0 +1,8 @@
+local config = {}
+
+config.lobby_addr = 'http://127.0.0.1'
+config.lobby_port = 80
+config.server_addr = 'http://127.0.0.1:8008'
+config.server_port = 8008
+
+return config
---
+++ dgreed/apps/morka/tutorials.h
@@ -0,0 +1,50 @@
+#ifndef TUTORIALS_H
+#define TUTORIALS_H
+
+#include <utils.h>
+#include "obj_types.h"
+
+typedef enum{
+	//NONE = 0,				// NULL event (never triggered)
+	AUTO,					// Happens automatically (always triggered)
+	MUSHROOM_IN_FRONT,		// Mushroom available in front of rabbit
+	MUSHROOM_BELOW,			// Mushroom below rabbit, possible to dive
+	BOUNCE_PERFORMED,		// Succesfull dive+bounce on a mushroom performed.
+	COMBO_X3,				// Combo x3 is performed
+	COINS_10,				// 10 Coins are collected
+
+	FINAL_STEP				// Indicates last step (never triggered)	
+} EventType;
+
+typedef struct {
+	const char* text;
+	const char* img;
+	const char* state;
+	Vector2 finger_pos;
+	bool pause_game;
+	bool force_input;
+	bool disable_input;
+
+	EventType show_on;
+	EventType dismiss_on;
+} TutorialStep;
+
+typedef struct {
+	const char* level_name;
+	TutorialStep* steps;
+} TutorialScenario;
+
+void tutorial_event(EventType e);
+void tutorials_init(void);
+void tutorials_close(void);
+void tutorials_set_level(const char* level);
+void tutorials_reset(ObjRabbit* r);
+bool tutorials_render(float t);
+
+void tutorials_enable(void);
+void tutorials_disable(void);
+bool tutorials_are_enabled(void);
+bool tutorials_show_unpaused(void);
+
+
+#endif
