ffa7dfac455cbefd599e2c6ab32cf8bc5c56214d
Andrius Mitkus <andrius.mitkus@gmail.com>
2013-02-22 14:50:29 +0200

    dgreed: optimize stb_truetype mem allocs

---
+++ dgreed/apps/morka/obj_types.h
@@ -0,0 +1,196 @@
+#ifndef OBJ_TYPES_H
+#define OBJ_TYPES_H
+
+#include <anim.h>
+
+#include "objects.h"
+
+enum {
+	OBJ_RABBIT_TYPE = 1,
+	OBJ_GROUND_TYPE,
+	OBJ_DECO_TYPE,
+	OBJ_FG_DECO_TYPE,
+	OBJ_MUSHROOM_TYPE,
+	OBJ_TRAMPOLINE_TYPE,
+	OBJ_CACTUS_TYPE,
+	OBJ_PIN,
+	OBJ_SPEED_TRIGGER_TYPE,
+	OBJ_FALL_TRIGGER_TYPE,
+	OBJ_PARTICLE_ANCHOR_TYPE,
+	OBJ_BG_PARTICLE_ANCHOR_TYPE,
+	OBJ_TOKEN_TYPE,
+	OBJ_FLOATER_TYPE
+};
+
+// Rabbit Data
+
+typedef struct {
+	uint combo_counter;
+	float last_keypress_t;
+	float last_keyrelease_t;
+	float jump_time;
+	float mushroom_hit_time;
+
+	bool virtual_key_up;
+	bool virtual_key_down;
+	bool virtual_key_pressed;
+
+	bool touching_ground;
+	bool jump_off_mushroom;
+	bool is_diving;
+	bool is_dead;
+	bool on_water;
+	Vector2 bounce_force;
+	bool player_control;
+	bool falling_down;
+	float land;
+	float rabbit_time;
+	const char* rabbit_name; 
+	bool game_over;
+	bool rubber_band;
+	int ai_max_combo;
+	int boost;
+	Color minimap_color;
+	float speed;
+	float xjump;
+	float yjump;
+	int tokens;
+	bool has_trampoline;
+	bool force_jump;
+	bool force_dive;
+
+	uint last_frame;
+} ObjRabbitData;
+
+typedef void (*ControlCallback)(GameObject* self);
+
+// Rabbit
+
+typedef struct {
+	GameObject header;
+	Anim* anim;
+	ControlCallback control;
+	ObjRabbitData* data;
+} ObjRabbit;
+
+extern GameObjectDesc obj_rabbit_desc;
+
+// Ground
+
+typedef struct {
+	GameObject header;
+} ObjGround;
+
+extern GameObjectDesc obj_ground_desc;
+
+// Background deco (static image, destroys itself when not visible)
+
+typedef struct {
+	GameObject header;
+} ObjDeco;
+
+extern GameObjectDesc obj_deco_desc;
+
+// Foreground Deco (static image, destroys itself when not visible)
+
+typedef struct {
+	GameObject header;
+} ObjFgDeco;
+
+extern GameObjectDesc obj_fg_deco_desc;
+
+// Mushroom
+
+typedef struct {
+	GameObject header;
+	float oh, h, dh;
+} ObjMushroom;
+
+extern GameObjectDesc obj_mushroom_desc;
+
+// Cactus (Spikeshroom)
+
+typedef struct {
+	GameObject header;
+	float t0,t1,d;
+	RectF original;
+	float damage;
+} ObjCactus;
+
+extern GameObjectDesc obj_cactus_desc;
+
+// Trampoline
+
+typedef struct {
+	GameObject header;
+	float oh, h, dh, db;
+	GameObject * owner;
+} ObjTrampoline;
+
+extern GameObjectDesc obj_trampoline_desc;
+
+// Race marker pin
+
+typedef struct {
+	GameObject header;
+	float speed;
+} ObjPin;
+
+extern GameObjectDesc obj_pin_desc;
+
+// Fall trigger
+
+typedef struct {
+	GameObject header;
+} ObjFallTrigger;
+
+extern GameObjectDesc obj_fall_trigger_desc;
+
+// Speed trigger
+
+typedef struct {
+	GameObject header;
+	float drag_coef;
+} ObjSpeedTrigger;
+
+extern GameObjectDesc obj_speed_trigger_desc;
+
+// Particle anchor
+
+typedef struct {
+	GameObject header;
+	Vector2 pos;
+	Vector2 screen_pos;
+} ObjParticleAnchor;
+
+extern GameObjectDesc obj_particle_anchor_desc;
+
+// Background Particle anchor
+
+typedef struct {
+	GameObject header;
+	Vector2 pos;
+	Vector2 screen_pos;
+} ObjBgParticleAnchor;
+
+extern GameObjectDesc obj_bg_particle_anchor_desc;
+
+// Token (coin)
+
+typedef struct {
+	GameObject header;
+	int value;
+} ObjToken;
+
+extern GameObjectDesc obj_token_desc;
+
+// Floating text/img object
+
+typedef struct {
+	GameObject header;
+} ObjFloater;
+
+extern GameObjectDesc obj_floater_desc;
+
+#endif
+
---
+++ dgreed/src/malka/ml_keyval.c
@@ -0,0 +1,118 @@
+#include "ml_common.h"
+
+#include "lua/lauxlib.h"
+#include "lua/lualib.h"
+
+#include <utils.h>
+#include <keyval.h>
+
+static int ml_keyval_init(lua_State* l) {
+	checkargs(1, "keyval.init");
+
+	const char* filename = luaL_checkstring(l, 1);
+
+	keyval_init(filename);
+
+	return 0;
+}
+
+static int ml_keyval_close(lua_State* l) {
+	checkargs(0, "keyval.close");
+
+	keyval_close();
+
+	return 0;
+}
+
+static int ml_keyval_get(lua_State* l) {
+	checkargs(2, "keyval.get");
+
+	const char* key = luaL_checkstring(l, 1);
+
+	if(lua_isnumber(l, 2)) {
+		float def = lua_tonumber(l, 2);
+		lua_pushnumber(l, keyval_get_float(key, def));
+		return 1;
+	}
+
+	if(lua_isboolean(l, 2)) {
+		bool def = lua_toboolean(l, 2);
+		lua_pushboolean(l, keyval_get_bool(key, def));
+		return 1;
+	}
+
+	if(lua_isstring(l, 2)) {
+		const char* def = lua_tostring(l, 2);
+		lua_pushstring(l, keyval_get(key, def));
+		return 1;
+	}
+
+	return luaL_error(l, "bad keyval.get default value type");
+}
+
+static int ml_keyval_set(lua_State* l) {
+	checkargs(2, "keyval.set");
+
+	const char* key = luaL_checkstring(l, 1);
+
+	if(lua_isnumber(l, 2)) {
+		float val = lua_tonumber(l, 2);
+		keyval_set_float(key, val);
+		return 0;
+	}
+
+	if(lua_isboolean(l, 2)) {
+		bool val = lua_toboolean(l, 2);
+		keyval_set_bool(key, val);
+		return 0;
+	}
+
+	if(lua_isstring(l, 2)) {
+		const char* val = lua_tostring(l, 2);
+		keyval_set(key, val);
+		return 0;
+	}
+
+	return luaL_error(l, "bad keyval.set value type");
+}
+
+static int ml_keyval_flush(lua_State* l) {
+	checkargs(0, "keyval.flush");
+
+	keyval_flush();
+
+	return 0;
+}
+
+static int ml_keyval_gc(lua_State* l) {
+	checkargs(0, "keyval.gc");
+
+	keyval_gc();
+
+	return 0;
+}
+
+static int ml_keyval_wipe(lua_State* l) {
+	checkargs(0, "keyval.wipe");
+
+	keyval_wipe();
+
+	return 0;
+}
+
+static const luaL_Reg keyval_fun[] = {
+	{"init", ml_keyval_init},
+	{"close", ml_keyval_close},
+	{"get", ml_keyval_get},
+	{"set", ml_keyval_set},
+	{"flush", ml_keyval_flush},
+	{"gc", ml_keyval_gc},
+	{"wipe", ml_keyval_wipe},
+	{NULL, NULL}
+};
+
+int malka_open_keyval(lua_State* l) {
+	malka_register(l, "keyval", keyval_fun);
+	return 1;
+}
+
