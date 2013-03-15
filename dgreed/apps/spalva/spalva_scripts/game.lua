5b2beae23a3aa3e39b6413c1f34e9a99a5754ab8
Andrius Mitkus <andrius.mitkus@gmail.com>
2013-03-15 16:30:00 +0200

    spalva: background colours

--- dgreed/apps/spalva/spalva_scripts/game.lua
+++ dgreed/apps/spalva/spalva_scripts/game.lua
@@ -7,6 +7,21 @@ local level = nil
 local char = nil
 local tileset = nil
 
+local background = rgba(1, 1, 1)
+local camera_pos = nil
+
+local colors = {
+	rgba(1, 0, 0),
+	rgba(1, 0.5, 0),
+	rgba(1, 1, 0),
+	rgba(0, 1, 0),
+	rgba(0, 1, 1),
+	rgba(0, 0, 1),
+	rgba(0.5, 0, 0.5),
+	rgba(1, 1, 1),
+	rgba(0, 0, 0)
+}
+
 function game.init()
 	tileset = tex.load(asset_dir..'tileset.png')
 	game.reset(levels[1])
@@ -24,7 +39,7 @@ function game.reset(desc)
 	end
 
 	local w, h = desc[1]:len(), #desc
-	level = tilemap.new(tile_size, tile_size, w, h, 1)
+	level = tilemap.new(tile_size, tile_size, w, h+1, 1)
 	tilemap.set_tileset(level, 0, tileset)
 
 	local ord_0 = string.byte('0')
@@ -35,6 +50,7 @@ function game.reset(desc)
 				-- nothing
 			elseif c == 's' then
 				local p = vec2(x, y) * tile_size
+				camera_pos = p
 				tilemap.set_camera(level, p, 1, 0)
 				char = character:new({pos = p})
 			else
@@ -50,7 +66,17 @@ function game.reset(desc)
 end
 
 function game.update()
-	char:update(level)
+	local c = char:update(level)
+	if c then
+		background = colors[c]
+	end
+
+
+	local p = vec2()
+	p.x = lerp(camera_pos.x, char.pos.x, 0.1)
+	p.y = lerp(camera_pos.y, char.pos.y, 0.03)
+	tilemap.set_camera(level, p, 1, 0)
+	camera_pos = p
 
 	if key.up(key.quit) then
 		states.pop()
@@ -59,6 +85,8 @@ function game.update()
 end
 
 function game.render(t)
+	sprsheet.draw('background', 0, scr_rect, background)
+
 	if level then
 		tilemap.render(level, scr_rect)
 		char:render(level)
