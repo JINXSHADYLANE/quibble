2e9f0f3468d6b72f7fa94396cb1ecafa406b2f6f
Domas Markevicius <domas.marko@gmail.com>
2013-03-22 16:11:50 +0200

    spalva: sprites for ending

---
+++ dgreed/apps/spalva/spalva_scripts/character.lua
@@ -0,0 +1,126 @@
+local character = {}
+local character_mt = {}
+character_mt.__index = character
+
+local gravity = 0.3
+
+local function controls1(self)
+	if key.pressed(key._left) then
+		self.vel.x = self.vel.x - self.move_acc
+		self.dir = true
+	end
+	if key.pressed(key._right) then
+		self.vel.x = self.vel.x + self.move_acc
+		self.dir = false
+	end
+	if key.down(key._up) and self.ground then
+		self.ground = false
+		self.vel.y = -self.jump_acc
+
+		if math.abs(self.vel.x) > 0.05 then
+			anim.play(self.anim, 'jump')
+		else
+			anim.play(self.anim, 'stand_jump')
+		end
+	end
+end
+
+function character:new(obj)
+	local o = {
+		width = 26,
+		height = 30,
+		move_acc = 0.5,
+		move_damp = 0.8,
+		jump_acc = 6.0,
+
+		pos = obj.pos + vec2(32/2, 32/2),
+		vel = vec2(0, 0),
+		dir = false,
+		ground = true,
+		frame = 0,
+		sprite = 'char',
+		anim = anim.new('char'),
+		controls = controls1,
+		color = i
+	}
+	setmetatable(o, character_mt)
+	return o
+end
+
+function character:update(level, world_bottom)
+	self:controls()
+
+	self.vel.y = self.vel.y + gravity
+	self.vel.x = self.vel.x * self.move_damp
+
+	local bbox = rect(
+		self.pos.x - self.width / 2,
+		self.pos.y - self.height / 2
+	)
+	bbox.r = bbox.l + self.width
+	bbox.b = bbox.t + self.height
+
+	local dx = tilemap.collide_swept(level, bbox, vec2(self.vel.x, 0))
+	self.pos = self.pos + dx
+	bbox.l = bbox.l + dx.x
+	bbox.r = bbox.r + dx.x
+	self.vel.x = dx.x
+	
+	local dy = tilemap.collide_swept(level, bbox, vec2(0, self.vel.y))
+	local was_on_ground = self.ground
+	if self.vel.y > 0 then
+		self.ground = dy.y == 0
+	end
+
+	if (not was_on_ground) and self.ground then
+		anim.play(self.anim, 'land')
+		self.walking = false
+	end
+
+	self.pos = self.pos + dy
+	self.vel.y = dy.y
+	bbox.t = bbox.t + dy.y
+	bbox.b = bbox.b + dy.y
+	self.bbox = bbox
+
+	if self.ground and bbox.b + 2 < world_bottom then
+		if math.abs(self.vel.x) > 0.05 then
+			if not self.walking then
+				anim.play(self.anim, 'run')
+				self.walking = true
+			end
+		else
+			anim.play(self.anim, 'stand')
+			self.walking = false
+		end
+
+		-- return color of tile we're standing on
+		local p = vec2((bbox.l + bbox.r) / 2, bbox.b + 2)
+		p.x = math.floor(p.x / 32)
+		p.y = math.floor(p.y / 32)
+		local tileset, tile = tilemap.tile(level, p.x, p.y, 0)
+		if tile > 0 and tile < 10 then
+			return tile
+		end
+	end
+end
+
+function character:render(level, world_bottom)
+	local b = self.bbox
+	if b then
+		local dest = rect(
+			b.l - 3, b.t - 2, b.r + 3, b.b	
+		)
+		local pos = tilemap.world2screen(level, scr_rect, dest)
+		if self.dir then
+			pos.l, pos.r = pos.r, pos.l
+		end
+
+		local spr = self.sprite
+
+		local f = anim.frame(self.anim)
+		sprsheet.draw_anim(spr, f, character_layer, pos)
+	end
+end
+
+return character
---
+++ dgreed/src/sprsheet.h
@@ -0,0 +1,80 @@
+#ifndef SPRSHEET_H
+#define SPRSHEET_H
+
+#include "system.h"
+
+// A layer on top of textures to stop worrying about loading, freeing 
+// and source rectangles.
+
+/*
+Example description:
+
+(sprsheet bulb
+	(prefix "bulb_assets/")
+	(preload background,title,robot)
+	(scale 1)	# scale all sprites by this factor when rendering
+	
+	(img background
+		(tex "background.png")
+	)
+
+	(img title
+		(tex "atlas.png")
+		(src 0,0,480,64)
+	)
+
+	(img off_center
+		(tex "pix.png")
+		(src 10,0,20,10)
+		(cntr 3,3)
+	)
+
+	(anim robot
+		(tex "robot.png")
+		(frames 12)
+		(src 0,0,32,48) # first frame
+		(offset 34,50) # offset for next frames
+		(grid 8,5) # frames grid size
+	)
+)
+
+*/
+
+typedef size_t SprHandle;
+
+void sprsheet_init(const char* desc);
+void sprsheet_close(void);
+
+SprHandle sprsheet_get_handle(const char* name);
+
+void sprsheet_get(const char* name, TexHandle* tex, RectF* src);
+void sprsheet_get_h(SprHandle handle, TexHandle* tex, RectF* src);
+
+void sprsheet_get_ex(const char* name, TexHandle* tex, RectF* src, Vector2* cntr_off);
+void sprsheet_get_ex_h(SprHandle handle, TexHandle* tex, RectF* src, Vector2* cntr_off);
+
+void sprsheet_get_anim(const char* name, uint frame, TexHandle* tex, RectF* src);
+void sprsheet_get_anim_h(SprHandle handle, uint frame, TexHandle* tex, RectF* src);
+
+uint sprsheet_get_anim_frames(const char* name);
+uint sprsheet_get_anim_frames_h(SprHandle handle);
+
+Vector2 sprsheet_get_size(const char* name);
+Vector2 sprsheet_get_size_h(SprHandle handle);
+
+// Helper rendering methods:
+
+void spr_draw(const char* name, uint layer, RectF dest, Color tint);
+void spr_draw_h(SprHandle handle, uint layer, RectF dest, Color tint);
+void spr_draw_anim(const char* name, uint frame, uint layer, RectF dest, Color tint);
+void spr_draw_anim_h(SprHandle handle, uint frame, uint layer, RectF dest, Color tint);
+void spr_draw_cntr(const char* name, uint layer, Vector2 dest, float rot, 
+		float scale, Color tint);
+void spr_draw_cntr_h(SprHandle handle, uint layer, Vector2 dest, float rot, 
+		float scale, Color tint);
+void spr_draw_anim_cntr(const char* name, uint frame, uint layer, Vector2 dest, 
+		float rot, float scale, Color tint);
+void spr_draw_anim_cntr_h(SprHandle handle, uint frame, uint layer, Vector2 dest, 
+		float rot, float scale, Color tint);
+
+#endif
