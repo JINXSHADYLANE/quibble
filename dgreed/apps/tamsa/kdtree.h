036996f52f61b1828578de97cc2246d657cb9566
Andrius Mitkus <andrius.mitkus@gmail.com>
2013-04-05 18:35:05 +0300

    tamsa: ray packet tracing

--- dgreed/apps/tamsa/kdtree.h
+++ dgreed/apps/tamsa/kdtree.h
@@ -24,14 +24,14 @@ void kdtree_free(KdTree* tree);
 // Traces a packet of n rays and fills up hit surface info for each ray.
 // Destroys the data, puts hit positions in ox, oy and normals in dx, dy.
 void kdtree_trace_surface(
-	float* ox, float* oy, float* dx, float* dy, 
+	KdTree* tree, float* ox, float* oy, float* dx, float* dy, 
 	byte* surfaces, uint n
 );
 
 // Traces a packet of n shadow rays, returns 0 - 1 ratio
 // of how many hit the target at (ex, ey)
 float kdtree_trace_shadow(
-	float* ox, float* oy, float ex, float ey, uint n
+	KdTree* tree, float* ox, float* oy, float ex, float ey, uint n
 );
 
 
