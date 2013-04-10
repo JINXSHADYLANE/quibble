811376fd8cf6daefe1dc58125933b526dd85314a
Tadas <tadbask@gmail.com>
2013-04-10 16:07:05 +0300

    vfont: font texture scaling

--- dgreed/src/vfont.h
+++ dgreed/src/vfont.h
@@ -78,6 +78,7 @@ void vfont_close(void);
 
 void vfont_select(const char* font_name, float size);
 void vfont_draw(const char* string, uint layer, Vector2 topleft, Color tint);
+void vfont_draw_ex(const char* string, uint layer, Vector2 topleft, Color tint, float scale);
 void vfont_draw_input(const char* string, uint layer, Vector2 topleft, Color tint);
 void vfont_precache(const char* string);
 void vfont_cache_invalidate(const char* string);
