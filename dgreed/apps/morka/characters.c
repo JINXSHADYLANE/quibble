e59cbc5698df031534d0b63c914fd024b54974cb
Domas Markevicius <domas.marko@gmail.com>
2013-04-17 15:12:28 +0300

    morka: planet centers

---
+++ dgreed/apps/morka/characters.c
@@ -0,0 +1,55 @@
+#include "characters.h"
+
+CharacterDefaultParams default_characters[character_count] = {
+	{
+	"Rabbit",
+	"rabbit_icon",
+	"rabbit",
+	"rabbit",
+	"Sprinter",
+	0,
+	COLOR_RGBA(150, 150, 150, 255),
+	550.0f,
+	100.0f,
+	400.0f
+	},
+
+	{
+	"Squirrel",
+	"squirrel_icon",
+	"squirrel",
+	"squirrel",
+	"Jumper",
+	49,	
+	COLOR_RGBA(255, 255, 0, 255),
+	490.0f,
+	120.0f,
+	420.0f	
+	},
+
+	{
+	"Ocean king",
+	"rabbit_icon",
+	"rabbit_3",
+	"rabbit",
+	"Swimmer",
+	99,	
+	COLOR_RGBA(0, 0, 255, 255),
+	500.0f,
+	100.0f,
+	400.0f		
+	},
+
+	{
+	"Skunk",
+	"rabbit_icon",
+	"rabbit_4",
+	"rabbit",
+	"Tough",
+	149,	
+	COLOR_RGBA(255, 0, 0, 255),
+	500.0f,
+	100.0f,
+	400.0f		
+	}			
+};
