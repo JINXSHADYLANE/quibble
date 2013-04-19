0b0326b5e8a0b514ea4cdddc0116ef1d58326de0
Domas Markevicius <domas.marko@gmail.com>
2013-04-19 18:20:23 +0300

    morka: golden rabbit assets

---
+++ dgreed/apps/morka/characters.c
@@ -0,0 +1,55 @@
+#include "characters.h"
+
+CharacterDefaultParams default_characters[character_count] = {
+	{
+	"Rabbit",
+	"rabbit_icon",
+	"rabbit",
+	"rabbit",
+	"Sprinter",
+	0,
+	COLOR_RGBA(150, 150, 150, 255),
+	550.0f,
+	100.0f,
+	400.0f
+	},
+
+	{
+	"Squirrel",
+	"squirrel_icon",
+	"squirrel",
+	"squirrel",
+	"Jumper",
+	49,	
+	COLOR_RGBA(254, 100, 0, 255),
+	490.0f,
+	120.0f,
+	420.0f	
+	},
+
+	{
+	"Ocean king",
+	"rabbit_icon",
+	"golden_rabbit",
+	"rabbit",
+	"Swimmer",
+	99,	
+	COLOR_RGBA(0, 0, 255, 255),
+	500.0f,
+	100.0f,
+	400.0f		
+	},
+
+	{
+	"Skunk",
+	"skunk_icon",
+	"skunk",
+	"squirrel",
+	"Tough",
+	149,	
+	COLOR_RGBA(0, 0, 0, 255),
+	500.0f,
+	100.0f,
+	400.0f		
+	}			
+};
---
+++ dgreed/apps/states/states_scripts/spring.lua
@@ -0,0 +1,43 @@
+module(..., package.seeall)
+
+local img = nil
+
+function init()
+	img = tex.load(pre..'yerka2.png')
+	print('init spring')
+end
+
+function close()
+	tex.free(img)
+	print('close spring')
+end
+
+function enter()
+	print('enter spring')
+end
+
+function preenter()
+	print('preenter spring')
+end
+
+function postleave()
+	print('postleave spring')
+end
+
+function leave()
+	print('leave spring')
+end
+
+function update()
+	if mouse.down(mouse.primary) then
+		states.replace('summer')
+	end
+	return true
+end
+
+function render(t)
+	local sgn = t / math.abs(t+0.00001)
+	local tt = smoothstep(0, 1, math.abs(t)) * sgn
+	video.draw_rect(img, 0, vec2(31 + tt * 512, 61))
+	return true
+end
