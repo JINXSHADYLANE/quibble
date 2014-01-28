f3bc58d617ed236f16d8395dd62decca0da514c3
Andrius Mitkus <andrius.mitkus@gmail.com>
2014-01-28 10:35:38 +0200

    pyktis: controls in title screen, name capitalization

--- dgreed/apps/pyktis/pyktis_scripts/title.lua
+++ dgreed/apps/pyktis/pyktis_scripts/title.lua
@@ -15,13 +15,20 @@ function title.render(t)
 	sprsheet.draw('empty', 0, scr_rect, white)
 
 	vfont.select(fnt, 64)
-	vfont.draw('pyktis', 1, vec2(48, 48), black)
+	vfont.draw('Pyktis', 1, vec2(48, 48), black)
 	vfont.select(fnt, 32)
 	vfont.draw('a puzzle for two players', 1, vec2(48, 112), grey)
 	vfont.select(fnt, 24)
 	local txt = 'press space to play'
 	local size = vfont.size(txt)
-	vfont.draw(txt, 1, vec2(scr_size.x/2, 350) - size*0.5, grey)
+	vfont.draw(txt, 1, vec2(scr_size.x/2, 410) - size*0.5, grey)
+
+	vfont.draw('Controls:', 1, vec2(410, 148), grey)
+	vfont.draw('arrows - first player', 1, vec2(410, 175), grey)
+	vfont.draw('w, a, s, d - second player', 1, vec2(410, 200), grey)
+	vfont.draw('space - undo', 1, vec2(410, 225), grey)
+	vfont.draw('esc - reset puzzle', 1, vec2(410, 250), grey)
+
 	vfont.select(fnt, 40)
 
 	return true
