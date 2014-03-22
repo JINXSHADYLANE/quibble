825bb05e431f122d7a9c6288e162dde1674fcf8b
Andrius Mitkus <andrius.mitkus@gmail.com>
2014-03-22 17:16:30 +0200

    vincas: player object

--- dgreed/apps/vincas/vincas_scripts/game.lua
+++ dgreed/apps/vincas/vincas_scripts/game.lua
@@ -1,18 +1,32 @@
 local game = {}
 
 local objects = require('objects')
+local player = require('player')
 
 function game.init()
+	objects.reset()
+	game.camera = rect(0, 0, scr_size.x, scr_size.y)
+
+	objects.add(player.new(vec2(scr_size.x / 2, 480)))
 end
 
 function game.close()
+	objects.close()
 end
 
 function game.update()
+	objects.update()
+
+	-- camera follows player
+	game.camera.b = objects.player.pos.y + 80
+	game.camera.t = game.camera.b - scr_size.y
+
 	return true
 end
 
 function game.render(t)
+	objects.render(game.camera, 3)
+
 	return true
 end
 
