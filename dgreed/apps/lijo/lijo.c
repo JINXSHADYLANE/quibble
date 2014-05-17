4d2be350e45d44745c0b1789561dc41c2486edb4
Andrius Mitkus <andrius.mitkus@gmail.com>
2014-05-17 15:52:40 +0300

    lijo: rebooting lietus

---
+++ dgreed/apps/lijo/lijo.c
@@ -0,0 +1,16 @@
+#include <malka/malka.h>
+#include <malka/ml_states.h>
+#include <vfont.h>
+
+extern bool draw_gfx_debug;
+
+int dgreed_main(int argc, const char** argv) {
+	draw_gfx_debug = false;
+	malka_init();
+	//vfont_init_ex(512, 512);
+	malka_params(argc, argv);
+	int res = malka_states_run("lijo_scripts/main.lua");	
+	malka_close();
+	return res;
+}
+
